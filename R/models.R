97b953bb69515e130f3468751093bf27971189d9
Michelle Donzallaz <michelle.donzallaz@ggaweb.ch>
2022-10-04 14:57:34 +0200

    change Andrew to model: Simplify Ttranform (remove get_names)

--- R/models.R
+++ R/models.R
@@ -73,36 +73,26 @@ rLNR <- function(lR,pars,p_types=c("m","s","t0")){
 #' @examples
 lnrMS <- function() {
 list(
-    type="RACE",
-    p_types=c("m","s","t0"),
-    Ntransform=function(x) {
-      # Transform to natural scale
-
-      get_p_types <- function(nams)
-        unlist(lapply(strsplit(nams,"_"),function(x){x[[1]]}))
-
-      if (!is.matrix(x)) {
-        nams <- get_p_types(names(x))
-        x[nams != "m"] <- exp(x[nams != "m"])
-      } else {
-        nams <- get_p_types(dimnames(x)[[2]])
-        x[,nams != "m"] <- exp(x[,nams != "m"])
-      }
-      x
-    },
-    # p_vector transform scaling parameter by s=1 assumed in lnr.R
-    transform = function(x) x,
-    # Trial dependent parameter transform
-    Ttransform = function(pars,dadm) pars,
-    # Random function for racing accumulators
-    rfun=function(lR,pars) rLNR(lR,pars),
-    # Density function (PDF) for single accumulator
-    dfun=function(rt,pars) dLNR(rt,pars),
-    # Probability function (CDF) for single accumulator
-    pfun=function(rt,pars) pLNR(rt,pars),
-    # Race likelihood combining pfun and dfun
-    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10))
-      log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+  type="RACE",
+  p_types=c("m","s","t0"),
+  Ntransform=function(x) {
+    # Transform to natural scale
+    x[,dimnames(x)[[2]] != "m"] <- exp(x[,dimnames(x)[[2]] != "m"])
+    x
+  },
+  # p_vector transform scaling parameter by s=1 assumed in lnr.R
+  transform = function(x) x,
+  # Trial dependent parameter transform
+  Ttransform = function(pars,dadm) pars,
+  # Random function for racing accumulators
+  rfun=function(lR,pars) rLNR(lR,pars),
+  # Density function (PDF) for single accumulator
+  dfun=function(rt,pars) dLNR(rt,pars),
+  # Probability function (CDF) for single accumulator
+  pfun=function(rt,pars) pLNR(rt,pars),
+  # Race likelihood combining pfun and dfun
+  log_likelihood=function(p_vector,dadm,min_ll=log(1e-10))
+    log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
   )
   }
 
