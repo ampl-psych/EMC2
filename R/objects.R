55ec8a5254142b7e1a64fcfd3a30234903ad6de0
Michelle Donzallaz <michelle.donzallaz@ggaweb.ch>
2022-10-19 23:06:34 +0200

    added mcmc extraction functions

--- R/objects.R
+++ R/objects.R
@@ -95,3 +95,172 @@ merge_samples <- function(samples){
   out_samples$samples <- sampled_objects
   return(out_samples)
 }
+
+
+#### mcmc extraction ----
+
+as_Mcmc <- function(sampler,filter=stages,thin=1,subfilter=0,
+                    selection=c("alpha","mu","variance","covariance","correlation","LL","epsilon")[1])
+  # replacement for pmwg package as_mccmc
+  # allows for selection of subj_ll, and specifying selection as an integer
+  # allows filter as single integer so can remove first filter samples
+  # adds ability to thin and to subfilter (i.e., remove trials after removing
+  # a stage), range of values or single integer (expanded to end)
+{
+
+  shorten <- function(x,r,d) {
+    if (!is.numeric(r)) stop("subfilter must be numeric\n")
+    if (length(r)==1) r <- (r+1):dim(x)[d]
+    if (min(r)<1 || max(r)>dim(x)[d]) stop("subfilter out of range\n")
+    if (d==2) x[,r,drop=FALSE] else x[,,r,drop=FALSE]
+  }
+
+  if (class(sampler) != "pmwgs") stop("sampler must be an pmwgs object\n")
+  stages <- c("burn", "adapt", "sample")
+  if (is.numeric(filter) && length(filter==1)) filter <- filter:sampler$samples$idx
+  if (is.numeric(selection))
+    selection <- selection=c("alpha","mu","variance","covariance","correlation","LL")[selection]
+  if (selection %in% c("mu","variance","covariance","correlation") &&
+      all(is.na((sampler$samples$theta_mu))))
+    stop("Cannot select mu, variance, covariance or correlation for non-hierarchical model\n")
+  if (all(filter %in% stages)) {
+    filter <- which(sampler$samples$stage %in% filter)
+  } else if (!all(filter %in% 1:sampler$samples$idx)) {
+    stop("filter is not a vector of stage names, or integer vector of indices\n")
+  }
+
+  if (selection == "mu") {
+    mu <- sampler$samples$theta_mu[, filter,drop=FALSE]
+    if (is.null(subfilter)) mu <- t(mu) else
+      mu <- t(shorten(mu,subfilter,2))
+    if (thin > dim(mu)[1]) stop("Thin to large\n")
+    out <- coda::mcmc(mu[seq(thin,dim(mu)[1],by=thin),,drop=FALSE])
+    attr(out,"selection") <- selection
+    return(out)
+  } else if (selection %in% c("variance","covariance","correlation")) {
+    tvar <- sampler$samples$theta_var[, , filter,drop=FALSE]
+    if (selection=="correlation")
+      tvar <- array(apply(tvar,3,stats::cov2cor),dim=dim(tvar),dimnames=dimnames(tvar))
+    if (selection %in% c("covariance","correlation")) {
+      lt <- lower.tri(tvar[,,1])
+      pnams <- dimnames(tvar)[[1]]
+      pnams <- outer(pnams,pnams,paste,sep=".")[lt]
+      tvar <- apply(tvar,3,function(x){x[lt]})
+      dimnames(tvar)[[1]] <- pnams
+      if (all(tvar==0))
+        stop("All covariances/correlations zero, model assumes independence.")
+    } else tvar <- apply(tvar,3,diag)
+    if (is.null(subfilter)) tvar <- t(tvar) else
+      tvar <- t(shorten(tvar,subfilter,2))
+    if (thin > dim(tvar)[1]) stop("Thin to large\n")
+    out <- coda::mcmc(tvar[seq(thin,dim(tvar)[1],by=thin),,drop=FALSE])
+    attr(out,"selection") <- selection
+    return(out)
+  } else if (selection == "alpha") {
+    alpha <- sampler$samples$alpha[, , filter,drop=FALSE]
+    if (!is.null(subfilter)) alpha <- shorten(alpha,subfilter,3)
+    if (thin > dim(alpha)[3]) stop("Thin to large\n")
+    alpha <- alpha[,,seq(thin,dim(alpha)[3],by=thin),drop=FALSE]
+    out <- stats::setNames(lapply(
+      seq(dim(alpha)[2]),
+      function(x) {
+        coda::mcmc(t(alpha[, x, ]))
+      }
+    ), names(sampler$data))
+    attr(out,"selection") <- selection
+    return(out)
+  } else if (selection %in% c("LL","epsilon")) {
+    if (selection == "epsilon")
+      LL <- sampler$samples$epsilon[, filter,drop=FALSE] else
+        LL <- sampler$samples$subj_ll[, filter,drop=FALSE]
+      if (!is.null(subfilter)) LL <- shorten(LL,subfilter,2)
+      if (thin > dim(LL)[2]) stop("Thin to large\n")
+      out <- setNames(lapply(
+        data.frame(t(LL[,seq(thin,dim(LL)[2],by=thin),drop=FALSE])),coda::mcmc),
+        names(sampler$data))
+      attr(out,"selection") <- selection
+      return(out)
+  }
+  stop("Argument `selection` should be one of mu, variance, covariance, correlation, alpha, LL, or epsilon\n")
+}
+
+
+
+
+# samplers=pmwg_mcmc
+as_mcmc.list <- function(samplers,
+                         selection=c("alpha","mu","variance","covariance","correlation","LL","epsilon")[1],
+                         filter="burn",thin=1,subfilter=0,mapped=FALSE,include_constants=FALSE)
+  # Combines as_Mcmc of samplers and returns mcmc.list
+  # mapped = TRUE map mu or alpha to model parameters (constants indicated by
+  # attribute isConstant)
+  # include_constants= TRUE, if not mapped add in constants
+{
+
+  stages <- c("burn", "adapt", "sample")
+  if (length(filter)>1) filter <- filter[1]
+  if (!class(samplers)=="list" || !all(unlist(lapply(samplers,class))=="pmwgs"))
+    stop("samplers must be a list of pmwgs objects")
+  if (!all(apply(do.call(rbind,lapply(samplers,function(x){
+    x$par_names})),2,function(x){all(x[1]==x[-1])})))
+    stop("samplers must have the same parameter names")
+  if (!all(apply(do.call(rbind,lapply(samplers,function(x){
+    x$subjects})),2,function(x){all(x[1]==x[-1])})))
+    stop("samplers must have the same subjects")
+
+  subjects <- names(samplers[[1]]$data)
+  mcmcList <- lapply(samplers,as_Mcmc,selection=selection,filter=filter,
+                     thin=thin,subfilter=subfilter)
+  if (mapped) {
+    if (selection == "alpha") {
+      mcmcList <- lapply(mcmcList,function(x){lapply(x,map_mcmc, include_constants = include_constants,
+                                                     design=attr(samplers,"design_list")[[1]],model=attr(samplers,"model_list")[[1]])})
+    } else if (selection=="mu") {
+      mcmcList <- lapply(mcmcList,map_mcmc, include_constants = include_constants,
+                         design=attr(samplers,"design_list")[[1]],model=attr(samplers,"model_list")[[1]])
+    } else warning("Can only map alpha or mu to model parameterization")
+  }
+  if (include_constants) {
+    if (selection == "alpha") {
+      mcmcList <- lapply(mcmcList,function(x){lapply(x,add_constants_mcmc,
+                                                     constants=attr(samplers,"design_list")[[1]]$constants)})
+    } else if (selection=="mu") {
+      mcmcList <- lapply(mcmcList,add_constants_mcmc,
+                         constants=attr(samplers,"design_list")[[1]]$constants)
+    }
+  }
+  if (selection %in% c("LL","epsilon"))
+    iter <- unlist(lapply(mcmcList,function(x){length(x[[1]])})) else
+      if (selection == "alpha")
+        iter <- unlist(lapply(mcmcList,function(x){dim(x[[1]])[1]})) else
+          iter <- unlist(lapply(mcmcList,function(x){dim(x)[1]}))
+  if (!all(iter[1]==iter[-1]))
+    message("Chains have different numbers of samples, using first ",min(iter))
+  iter <- min(iter)
+  if (selection %in% c("alpha","LL","epsilon")) {
+    nChains <- length(mcmcList)
+    ns <- length(samplers[[1]]$subjects)
+    out <- vector(mode="list",length=ns)
+    lst <- vector(mode="list",length=nChains)
+    for (i in 1:ns) {
+      for (j in 1:nChains) if (selection %in% c("LL","epsilon"))
+        lst[[j]] <- as.mcmc(mcmcList[[j]][[i]][1:iter]) else {
+          isConstant <- attr(mcmcList[[j]][[i]],"isConstant")
+          lst[[j]] <- as.mcmc(mcmcList[[j]][[i]][1:iter,])
+          attr(lst[[j]],"isConstant") <- isConstant
+        }
+      out[[i]] <- mcmc.list(lst)
+    }
+    out <- setNames(out,subjects)
+    attr(out,"selection") <- selection
+    return(out)
+  } else {
+    out <- mcmc.list(mcmcList)
+    attr(out,"selection") <- selection
+    return(out)
+  }
+}
+
+
+
+
