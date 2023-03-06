f6ac9f238569593267df2c359b64c5050b5e71c3
AJHgit <andrew.heathcote@utas.edu.au>
2023-03-06 14:07:45 +0100

    Commented out functions moved to C++, changed from tpos to standard ok notation in pLBA and dLBA

--- R/model_LBA.R
+++ R/model_LBA.R
@@ -1,62 +1,109 @@
+# # Moved to C+_ in model_LBA.cpp
+#
+# pnormP <- function (x, mean = 0, sd = 1, lower.tail = TRUE)
+#       ifelse(abs(x) < 7, pnorm(x, mean = mean, sd = sd, lower.tail = lower.tail),
+#              ifelse(x < 0, 0, 1))
+#
+# dnormP <- function (x, mean = 0, sd = 1)
+#       ifelse(abs(x) < 7, dnorm(x, mean = mean, sd = sd), 0)
+#
+#
+# dlba_norm <- function (dt,A,b,v,sv,posdrift=TRUE,robust=FALSE)
+#     # like dlba_norm_core but t0 dealt with outside (removed from dt)
+# {
+#
+#
+#     if (robust) {
+#       pnorm1 <- pnormP
+#       dnorm1 <- dnormP
+#     } else {
+#       pnorm1 <- pnorm
+#       dnorm1 <- dnorm
+#     }
+#
+#     if (posdrift)
+#       denom <- pmax(pnorm1(v/sv), 1e-10) else
+#         denom <- rep(1, length(t))
+#
+#     A_small <- A < 1e-10
+#     if (any(A_small)) {
+#       out <- numeric(length(dt))
+#       out[A_small] <- pmax(0, ((b[A_small]/dt[A_small]^2) *
+#                                  dnorm1(b[A_small]/dt[A_small],v[A_small], sd = sv[A_small]))/denom[A_small])
+#       zs <- dt[!A_small] * sv[!A_small]
+#       zu <- dt[!A_small] * v[!A_small]
+#       chiminuszu <- b[!A_small] - zu
+#       chizu <- chiminuszu/zs
+#       chizumax <- (chiminuszu - A[!A_small])/zs
+#       out[!A_small] <- pmax(0, (v[!A_small] * (pnorm1(chizu) -
+#                                                  pnorm1(chizumax)) + sv[!A_small] * (dnorm1(chizumax) -
+#                                                                                        dnorm1(chizu)))/(A[!A_small] * denom[!A_small]))
+#       return(out)
+#     } else {
+#       zs <- dt * sv
+#       zu <- dt * v
+#       chiminuszu <- b - zu
+#       chizu <- chiminuszu/zs
+#       chizumax <- (chiminuszu - A)/zs
+#       return(pmax(0, (v * (pnorm1(chizu) - pnorm1(chizumax)) +
+#                         sv * (dnorm1(chizumax) - dnorm1(chizu)))/(A * denom)))
+#     }
+# }
+#
+#
+# plba_norm <- function (dt,A,b,v,sv,posdrift=TRUE,robust=FALSE)
+#     # like plba_norm_core but t0 dealt with outside (removed from dt)
+# {
+#
+#     if (robust) {
+#       pnorm1 <- pnormP
+#       dnorm1 <- dnormP
+#     } else {
+#       pnorm1 <- pnorm
+#       dnorm1 <- dnorm
+#     }
+#     if (posdrift)
+#       denom <- pmax(pnorm1(v/sv), 1e-10) else
+#         denom <- 1
+#     A_small <- A < 1e-10
+#     if (any(A_small)) {
+#       out <- numeric(length(dt))
+#       out[A_small] <- pmin(1, pmax(0, (pnorm1(b[A_small]/dt[A_small],
+#                                               mean = v[A_small], sd = sv[A_small],
+#                                               lower.tail = FALSE))/denom[A_small]))
+#       zs <- dt[!A_small] * sv[!A_small]
+#       zu <- dt[!A_small] * v[!A_small]
+#       chiminuszu <- b[!A_small] - zu
+#       xx <- chiminuszu - A[!A_small]
+#       chizu <- chiminuszu/zs
+#       chizumax <- xx/zs
+#       tmp1 <- zs * (dnorm1(chizumax) - dnorm1(chizu))
+#       tmp2 <- xx * pnorm1(chizumax) - chiminuszu * pnorm1(chizu)
+#       out[!A_small] <- pmin(pmax(0, (1 + (tmp1 + tmp2)/A[!A_small])/denom[!A_small]),1)
+#       return(out)
+#     } else {
+#       zs <- dt * sv
+#       zu <- dt * v
+#       chiminuszu <- b - zu
+#       xx <- chiminuszu - A
+#       chizu <- chiminuszu/zs
+#       chizumax <- xx/zs
+#       tmp1 <- zs * (dnorm1(chizumax) - dnorm1(chizu))
+#       tmp2 <- xx * pnorm1(chizumax) - chiminuszu * pnorm1(chizu)
+#       return(pmin(pmax(0, (1 + (tmp1 + tmp2)/A)/denom), 1))
+#     }
+# }
+#
+#
 dLBA <- function (rt, pars, posdrift = TRUE, robust = FALSE)
   # posdrift = truncated positive normal rates
   # robust slower, deals with extreme rate values
 {
-
-  dlba_norm <- function (dt,A,b,v,sv,posdrift=TRUE,robust=FALSE)
-    # like dlba_norm_core but t0 dealt with outside (removed from dt)
-  {
-
-    pnormP <- function (x, mean = 0, sd = 1, lower.tail = TRUE)
-      ifelse(abs(x) < 7, pnorm(x, mean = mean, sd = sd, lower.tail = lower.tail),
-             ifelse(x < 0, 0, 1))
-
-    dnormP <- function (x, mean = 0, sd = 1)
-      ifelse(abs(x) < 7, dnorm(x, mean = mean, sd = sd), 0)
-
-    if (robust) {
-      pnorm1 <- pnormP
-      dnorm1 <- dnormP
-    } else {
-      pnorm1 <- pnorm
-      dnorm1 <- dnorm
-    }
-
-    if (posdrift)
-      denom <- pmax(pnorm1(v/sv), 1e-10) else
-        denom <- rep(1, length(t))
-
-    A_small <- A < 1e-10
-    if (any(A_small)) {
-      out <- numeric(length(dt))
-      out[A_small] <- pmax(0, ((b[A_small]/dt[A_small]^2) *
-                                 dnorm1(b[A_small]/dt[A_small],v[A_small], sd = sv[A_small]))/denom[A_small])
-      zs <- dt[!A_small] * sv[!A_small]
-      zu <- dt[!A_small] * v[!A_small]
-      chiminuszu <- b[!A_small] - zu
-      chizu <- chiminuszu/zs
-      chizumax <- (chiminuszu - A[!A_small])/zs
-      out[!A_small] <- pmax(0, (v[!A_small] * (pnorm1(chizu) -
-                                                 pnorm1(chizumax)) + sv[!A_small] * (dnorm1(chizumax) -
-                                                                                       dnorm1(chizu)))/(A[!A_small] * denom[!A_small]))
-      return(out)
-    } else {
-      zs <- dt * sv
-      zu <- dt * v
-      chiminuszu <- b - zu
-      chizu <- chiminuszu/zs
-      chizumax <- (chiminuszu - A)/zs
-      return(pmax(0, (v * (pnorm1(chizu) - pnorm1(chizumax)) +
-                        sv * (dnorm1(chizumax) - dnorm1(chizu)))/(A * denom)))
-    }
-  }
-
-
   dt <- rt - pars[,"t0"]
-  tpos <- (dt>0) & (pars[,"b"] >= pars[,"A"])
+  ok <- (dt>0) & (pars[,"b"] >= pars[,"A"])
   out <- numeric(length(dt))
-  out[tpos] <- dlba_norm(dt = dt[tpos], A = pars[tpos,"A"], b = pars[tpos,"b"],
-                         v = pars[tpos,"v"], sv = pars[tpos,"sv"],
+  out[ok] <- dlba(t = dt[ok], A = pars[ok,"A"], b = pars[ok,"b"],
+                         v = pars[ok,"v"], sv = pars[ok,"sv"],
                          posdrift = posdrift, robust = robust)
   out
 }
@@ -66,63 +113,11 @@ pLBA <- function (rt, pars, posdrift = TRUE, robust = FALSE)
   # posdrift = truncated positive normal rates
   # robust slower, deals with extreme rate values
 {
-
-  plba_norm <- function (dt,A,b,v,sv,posdrift=TRUE,robust=FALSE)
-    # like plba_norm_core but t0 dealt with outside (removed from dt)
-  {
-
-    pnormP <- function (x, mean = 0, sd = 1, lower.tail = TRUE)
-      ifelse(abs(x) < 7, pnorm(x, mean = mean, sd = sd, lower.tail=lower.tail),
-             ifelse(x < 0, 0, 1))
-
-    dnormP <- function (x, mean = 0, sd = 1)
-      ifelse(abs(x) < 7, dnorm(x, mean = mean, sd = sd), 0)
-
-    if (robust) {
-      pnorm1 <- pnormP
-      dnorm1 <- dnormP
-    } else {
-      pnorm1 <- pnorm
-      dnorm1 <- dnorm
-    }
-    if (posdrift)
-      denom <- pmax(pnorm1(v/sv), 1e-10) else
-        denom <- 1
-    A_small <- A < 1e-10
-    if (any(A_small)) {
-      out <- numeric(length(dt))
-      out[A_small] <- pmin(1, pmax(0, (pnorm1(b[A_small]/dt[A_small],
-                                              mean = v[A_small], sd = sv[A_small],
-                                              lower.tail = FALSE))/denom[A_small]))
-      zs <- dt[!A_small] * sv[!A_small]
-      zu <- dt[!A_small] * v[!A_small]
-      chiminuszu <- b[!A_small] - zu
-      xx <- chiminuszu - A[!A_small]
-      chizu <- chiminuszu/zs
-      chizumax <- xx/zs
-      tmp1 <- zs * (dnorm1(chizumax) - dnorm1(chizu))
-      tmp2 <- xx * pnorm1(chizumax) - chiminuszu * pnorm1(chizu)
-      out[!A_small] <- pmin(pmax(0, (1 + (tmp1 + tmp2)/A[!A_small])/denom[!A_small]),1)
-      return(out)
-    } else {
-      zs <- dt * sv
-      zu <- dt * v
-      chiminuszu <- b - zu
-      xx <- chiminuszu - A
-      chizu <- chiminuszu/zs
-      chizumax <- xx/zs
-      tmp1 <- zs * (dnorm1(chizumax) - dnorm1(chizu))
-      tmp2 <- xx * pnorm1(chizumax) - chiminuszu * pnorm1(chizu)
-      return(pmin(pmax(0, (1 + (tmp1 + tmp2)/A)/denom), 1))
-    }
-  }
-
-
   dt <- rt - pars[,"t0"]
-  tpos <- (dt>0) & (pars[,"b"] >= pars[,"A"])
+  ok <- (dt>0) & (pars[,"b"] >= pars[,"A"])
   out <- numeric(length(dt))
-  out[tpos] <- plba_norm(dt = dt[tpos], A = pars[tpos,"A"], b = pars[tpos,"b"],
-                         v = pars[tpos,"v"], sv = pars[tpos,"sv"],
+  out[ok] <- plba(t = dt[ok], A = pars[ok,"A"], b = pars[ok,"b"],
+                         v = pars[ok,"v"], sv = pars[ok,"sv"],
                          posdrift = posdrift, robust = robust)
   out
 }
@@ -152,6 +147,12 @@ rLBA <- function(lR,pars,p_types=c("v","sv","b","A","t0"),posdrift = TRUE)
 }
 
 # lba_B parameterization
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 lbaB <- function(){
   list(
     type="RACE",
@@ -182,3 +183,71 @@ lbaB <- function(){
   )
 }
 
+# lba_B parameterization
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+albaB <- function(){
+  list(
+    type="RACE",
+    p_types=c("v_0","v_S","v_D","sv","B","A","t0"),
+    # Transform to natural scale
+    Ntransform=function(x) {
+      x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
+      x
+    },
+    # p_vector transform, sets sv as a scaling parameter
+    transform = function(p) {p},
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) {
+      pars <- cbind(pars,v = pars[,"v_0"] + pars[,"v_D"]*dadm$SD + pars[,"v_S"]*dadm$SS)
+      pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      pars
+
+    },
+    # Random function for racing accumulator
+    rfun=function(lR,pars) rLBA(lR,pars,posdrift=TRUE),
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10)){
+      log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+    }
+  )}
+
+# lba_B parameterization with sv=1 scaling
+MlbaB <- function(){
+  list(
+    type="RACE",
+    p_types=c("v","sv","B","A","t0"),
+    Ntransform=function(x) {
+      # Transform to natural scale
+      x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
+      x
+    },
+    # p_vector transform, sets sv as a scaling parameter
+    transform = function(p) p,
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) {
+      pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      pars
+    },
+    # Random function for racing accumulator
+    rfun=function(lR,pars) rLBA(lR,pars,posdrift=TRUE),
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10)){
+      log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+    }
+  )
+}
