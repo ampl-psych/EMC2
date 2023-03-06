467960c07d75fb39497b4d0c77b1bdfdcd491158
AJHgit <andrew.heathcote@utas.edu.au>
2023-03-06 14:11:02 +0100

    Commented out funcitons moved to C++, added protection for rt < t0, added roxygen skeletons to rdmB and rdmBt0natural

--- R/model_RDM.R
+++ R/model_RDM.R
@@ -19,203 +19,205 @@
 
 #### distribution functions
 
-
+# # Moved to C++ model_RDM.cpp
+#
+# digt0 <- function(t,k=1,l=1) {
+#       # pdf of inverse gaussian at t with no k variability
+#       # much faster than statmod's dinvgauss funciton
+#
+#       lambda <- k^2
+#       l0 <- l==0
+#       e <- numeric(length(t))
+#       if ( any(!l0) ) {
+#         mu <- k[!l0]/l[!l0]
+#         e[!l0] <- -(lambda[!l0]/(2*t[!l0])) * (t[!l0]^2/mu^2 - 2*t[!l0]/mu  + 1)
+#       }
+#       if ( any(l0) )  e[l0] <- -.5*lambda[l0]/t[l0]
+#       x <- exp(e + .5*log(lambda) - .5*log(2*t^3*pi))
+#       x[t<=0] <- 0
+#       x
+# }
+#
+#
+# digt <- function(t,k=1,l=1,a=.1,tiny=1e-10) {
+#     # pdf of inverse gaussian at t with k +/- a/2 uniform variability
+#     # returns digt0 if a<1e-10
+#
+#
+#     options(warn=-1)
+#     if(length(k)!=length(t)) k <- rep(k,length.out=length(t))
+#     if(length(l)!=length(t)) l <- rep(l,length.out=length(t))
+#     if(length(a)!=length(t)) a <- rep(a,length.out=length(t))
+#
+#     tpos <- t<=0
+#
+#     atiny <- a<=tiny & !tpos
+#     a[atiny] <- 0
+#
+#     ltiny <- (l<=tiny) & !atiny & !tpos
+#     notltiny <- (l>tiny) & !atiny & !tpos
+#     l[l<=tiny] <- 0
+#
+#     x <- numeric(length(t))
+#
+#     # No threshold variability
+#     if ( any(atiny) )
+#       x[atiny] <- digt0(t=t[atiny],k=k[atiny],l=l[atiny])
+#
+#     # Threshold variability
+#     if ( any(!atiny) ) {
+#
+#       if ( any(notltiny) ) { # rate non-zero
+#
+#         sqr.t <- sqrt(t[notltiny])
+#
+#         term.1a <- -(a[notltiny]-k[notltiny]+t[notltiny]*l[notltiny])^2/(2*t[notltiny])
+#         term.1b <- -(a[notltiny]+k[notltiny]-t[notltiny]*l[notltiny])^2/(2*t[notltiny])
+#         term.1 <- (exp(term.1a) - exp(term.1b))/sqrt(2*pi*t[notltiny])
+#
+#         term.2a <- log(.5)+log(l[notltiny])
+#         term.2b <- 2*pnorm((-k[notltiny]+a[notltiny])/sqr.t+sqr.t*l[notltiny])-1
+#         term.2c <- 2*pnorm((k[notltiny]+a[notltiny])/sqr.t-sqr.t*l[notltiny])-1
+#         term.2d <- term.2b+term.2c
+#         term.2 <- exp(term.2a)*term.2d
+#
+#         term.3 <- term.1+term.2
+#         term.4 <- log(term.3)-log(2)-log(a[notltiny])
+#         x[notltiny] <- exp(term.4)
+#       }
+#
+#       if ( any(ltiny) ) {  # rate zero
+#         log.t <- log(t[ltiny])
+#         term.1 <- -.5*(log(2)+log(pi)+log.t)
+#         term.2 <- (k[ltiny]-a[ltiny])^2/(2*t[ltiny])
+#         term.3 <- (k[ltiny]+a[ltiny])^2/(2*t[ltiny])
+#         term.4 <- (exp(-term.2)-exp(-term.3))
+#         term.5 <- term.1+log(term.4) - log(2) - log(a[ltiny])
+#         x[ltiny] <- exp(term.5)
+#       }
+#
+#     }
+#
+#     x[x<0 | is.nan(x) ] <- 0
+#     x
+# }
+#
+#
+# pigt0 <- function(t,k=1,l=1) {
+#       # cdf of inverse gaussian at t with no k variability
+#       # much faster than statmod's pinvgauss funciton
+#
+#       mu <- k/l
+#       lambda <- k^2
+#
+#       e <- exp(log(2*lambda) - log(mu))
+#       add <- sqrt(lambda/t) * (1 + t/mu)
+#       sub <- sqrt(lambda/t) * (1 - t/mu)
+#
+#       p.1 <- 1 - pnorm(add)
+#       p.2 <- 1 - pnorm(sub)
+#       x <- exp(e + log(p.1)) + p.2
+#
+#       x[t<0] <- 0
+#       x
+# }
+#
+#
+# pigt <- function(t,k=1,l=1,a=.1,tiny=1e-10) {
+#     # cdf of inverse gaussian at t with k +/- a/2 uniform variability
+#     # returns pigt0 if a<=0
+#
+#     options(warn=-1)
+#     if(length(k)!=length(t)) k <- rep(k,length.out=length(t))
+#     if(length(l)!=length(t)) l <- rep(l,length.out=length(t))
+#     if(length(a)!=length(t)) a <- rep(a,length.out=length(t))
+#
+#     tpos <- t<=0
+#
+#     atiny <- a<=tiny & !tpos
+#     a[atiny] <- 0
+#
+#     ltiny <- (l<=tiny) & !atiny & !tpos
+#     notltiny <- (l>tiny) & !atiny & !tpos
+#     l[l<=tiny] <- 0
+#
+#     x <- numeric(length(t))
+#
+#     # No threshold variability
+#     if ( any(atiny) )
+#       x[atiny] <- pigt0(t[atiny],k[atiny],l[atiny])
+#
+#     # Threshold variability
+#     if ( any(!atiny) ) {
+#
+#       if ( any(notltiny) ) { # rate non-zero
+#
+#         log.t <- log(t[notltiny])
+#         sqr.t <- sqrt(t[notltiny])
+#
+#         term.1a <- .5*log.t-.5*log(2*pi)
+#         term.1b <- exp(-((k[notltiny]-a[notltiny]-t[notltiny]*l[notltiny])^2/t[notltiny])/2)
+#         term.1c <- exp(-((k[notltiny]+a[notltiny]-t[notltiny]*l[notltiny])^2/t[notltiny])/2)
+#         term.1 <- exp(term.1a)*(term.1b-term.1c)
+#
+#         term.2a <- exp(2*l[notltiny]*(k[notltiny]-a[notltiny]) +
+#                          log(pnorm(-(k[notltiny]-a[notltiny]+t[notltiny]*l[notltiny])/sqr.t)))
+#         term.2b <- exp(2*l[notltiny]*(k[notltiny]+a[notltiny]) +
+#                          log(pnorm(-(k[notltiny]+a[notltiny]+t[notltiny]*l[notltiny])/sqr.t)))
+#         term.2 <- a[notltiny] + (term.2b-term.2a)/(2*l[notltiny])
+#
+#         term.4a <- 2*pnorm((k[notltiny]+a[notltiny])/sqr.t-sqr.t*l[notltiny])-1
+#         term.4b <- 2*pnorm((k[notltiny]-a[notltiny])/sqr.t-sqr.t*l[notltiny])-1
+#         term.4c <- .5*(t[notltiny]*l[notltiny] - a[notltiny] - k[notltiny] + .5/l[notltiny])
+#         term.4d <- .5*(k[notltiny] - a[notltiny] - t[notltiny]*l[notltiny] - .5/l[notltiny])
+#         term.4 <- term.4c*term.4a + term.4d*term.4b
+#
+#         x[notltiny] <- (term.4 + term.2 + term.1)/(2*a[notltiny])
+#       }
+#
+#       if ( any(ltiny) ) {  # rate zero
+#         sqr.t <- sqrt(t[ltiny])
+#         log.t <- log(t[ltiny])
+#         term.5a <- 2*pnorm((k[ltiny]+a[ltiny])/sqr.t)-1
+#         term.5b <- 2*pnorm(-(k[ltiny]-a[ltiny])/sqr.t)-1
+#         term.5 <- (-(k[ltiny]+a[ltiny])*term.5a - (k[ltiny]-a[ltiny])*term.5b)/(2*a[ltiny])
+#
+#         term.6a <- -.5*(k[ltiny]+a[ltiny])^2/t[ltiny] - .5*log(2) -.5*log(pi) + .5*log.t - log(a[ltiny])
+#         term.6b <- -.5*(k[ltiny]-a[ltiny])^2/t[ltiny] - .5*log(2) -.5*log(pi) + .5*log.t - log(a[ltiny])
+#         term.6 <- 1 + exp(term.6b) - exp(term.6a)
+#
+#         x[ltiny] <- term.5 + term.6
+#       }
+#
+#     }
+#
+#     x[x<0 | is.nan(x) ] <- 0
+#     x
+# }
+#
+#
 dRDM <- function(rt,pars)
   # density for single accumulator
 {
-
-  digt <- function(t,k=1,l=1,a=.1,tiny=1e-10) {
-    # pdf of inverse gaussian at t with k +/- a/2 uniform variability
-    # returns digt.0 if a<1e-10
-
-    digt.0 <- function(t,k=1,l=1) {
-      # pdf of inverse gaussian at t with no k variability
-      # much faster than statmod's dinvgauss funciton
-
-      lambda <- k^2
-      l0 <- l==0
-      e <- numeric(length(t))
-      if ( any(!l0) ) {
-        mu <- k[!l0]/l[!l0]
-        e[!l0] <- -(lambda[!l0]/(2*t[!l0])) * (t[!l0]^2/mu^2 - 2*t[!l0]/mu  + 1)
-      }
-      if ( any(l0) )  e[l0] <- -.5*lambda[l0]/t[l0]
-      x <- exp(e + .5*log(lambda) - .5*log(2*t^3*pi))
-      x[t<=0] <- 0
-      x
-    }
-
-    options(warn=-1)
-    if(length(k)!=length(t)) k <- rep(k,length.out=length(t))
-    if(length(l)!=length(t)) l <- rep(l,length.out=length(t))
-    if(length(a)!=length(t)) a <- rep(a,length.out=length(t))
-
-    tpos <- t<=0
-
-    atiny <- a<=tiny & !tpos
-    a[atiny] <- 0
-
-    ltiny <- (l<=tiny) & !atiny & !tpos
-    notltiny <- (l>tiny) & !atiny & !tpos
-    l[l<=tiny] <- 0
-
-    x <- numeric(length(t))
-
-    # No threshold variability
-    if ( any(atiny) )
-      x[atiny] <- digt.0(t=t[atiny],k=k[atiny],l=l[atiny])
-
-    # Threshold variability
-    if ( any(!atiny) ) {
-
-      if ( any(notltiny) ) { # rate non-zero
-
-        sqr.t <- sqrt(t[notltiny])
-
-        term.1a <- -(a[notltiny]-k[notltiny]+t[notltiny]*l[notltiny])^2/(2*t[notltiny])
-        term.1b <- -(a[notltiny]+k[notltiny]-t[notltiny]*l[notltiny])^2/(2*t[notltiny])
-        term.1 <- (exp(term.1a) - exp(term.1b))/sqrt(2*pi*t[notltiny])
-
-        term.2a <- log(.5)+log(l[notltiny])
-        term.2b <- 2*pnorm((-k[notltiny]+a[notltiny])/sqr.t+sqr.t*l[notltiny])-1
-        term.2c <- 2*pnorm((k[notltiny]+a[notltiny])/sqr.t-sqr.t*l[notltiny])-1
-        term.2d <- term.2b+term.2c
-        term.2 <- exp(term.2a)*term.2d
-
-        term.3 <- term.1+term.2
-        term.4 <- log(term.3)-log(2)-log(a[notltiny])
-        x[notltiny] <- exp(term.4)
-      }
-
-      if ( any(ltiny) ) {  # rate zero
-        log.t <- log(t[ltiny])
-        term.1 <- -.5*(log(2)+log(pi)+log.t)
-        term.2 <- (k[ltiny]-a[ltiny])^2/(2*t[ltiny])
-        term.3 <- (k[ltiny]+a[ltiny])^2/(2*t[ltiny])
-        term.4 <- (exp(-term.2)-exp(-term.3))
-        term.5 <- term.1+log(term.4) - log(2) - log(a[ltiny])
-        x[ltiny] <- exp(term.5)
-      }
-
-    }
-
-    x[x<0 | is.nan(x) ] <- 0
-    x
-  }
-
-  rt <- rt - pars[,"t0"]
   out <- numeric(length(rt))
-  ok <- !pars[,"v"] < 0  # code handles rate zero case
+  ok <- rt > pars[,"t0"] &
+        !pars[,"v"] < 0  # code handles rate zero case
   if (any(dimnames(pars)[[2]]=="s")) # rescale
-    pars[,c("A","B","v")] <- pars[,c("A","B","v")]/pars[,"s"]
-  out[ok] <- digt(rt[ok],k=pars[ok,"B"]+pars[ok,"A"]/2,l=pars[ok,"v"],a=pars[ok,"A"]/2)
+    pars[ok,c("A","B","v")] <- pars[ok,c("A","B","v")]/pars[ok,"s"]
+  out[ok] <- dWald(rt[ok],v=pars[ok,"v"],B=pars[ok,"B"],A=pars[ok,"A"],t0=pars[ok,"t0"])
   out
 }
-
-
 
 pRDM <- function(rt,pars)
   # cumulative density for single accumulator
 {
-  pigt <- function(t,k=1,l=1,a=.1,tiny=1e-10) {
-    # cdf of inverse gaussian at t with k +/- a/2 uniform variability
-    # returns pigt.0 if a<=0
-
-    pigt.0 <- function(t,k=1,l=1) {
-      # cdf of inverse gaussian at t with no k variability
-      # much faster than statmod's pinvgauss funciton
-
-      mu <- k/l
-      lambda <- k^2
-
-      e <- exp(log(2*lambda) - log(mu))
-      add <- sqrt(lambda/t) * (1 + t/mu)
-      sub <- sqrt(lambda/t) * (1 - t/mu)
-
-      p.1 <- 1 - pnorm(add)
-      p.2 <- 1 - pnorm(sub)
-      x <- exp(e + log(p.1)) + p.2
-
-      x[t<0] <- 0
-      x
-    }
-
-    options(warn=-1)
-    if(length(k)!=length(t)) k <- rep(k,length.out=length(t))
-    if(length(l)!=length(t)) l <- rep(l,length.out=length(t))
-    if(length(a)!=length(t)) a <- rep(a,length.out=length(t))
-
-    tpos <- t<=0
-
-    atiny <- a<=tiny & !tpos
-    a[atiny] <- 0
-
-    ltiny <- (l<=tiny) & !atiny & !tpos
-    notltiny <- (l>tiny) & !atiny & !tpos
-    l[l<=tiny] <- 0
-
-    x <- numeric(length(t))
-
-    # No threshold variability
-    if ( any(atiny) )
-      x[atiny] <- pigt.0(t[atiny],k[atiny],l[atiny])
-
-    # Threshold variability
-    if ( any(!atiny) ) {
-
-      if ( any(notltiny) ) { # rate non-zero
-
-        log.t <- log(t[notltiny])
-        sqr.t <- sqrt(t[notltiny])
-
-        term.1a <- .5*log.t-.5*log(2*pi)
-        term.1b <- exp(-((k[notltiny]-a[notltiny]-t[notltiny]*l[notltiny])^2/t[notltiny])/2)
-        term.1c <- exp(-((k[notltiny]+a[notltiny]-t[notltiny]*l[notltiny])^2/t[notltiny])/2)
-        term.1 <- exp(term.1a)*(term.1b-term.1c)
-
-        term.2a <- exp(2*l[notltiny]*(k[notltiny]-a[notltiny]) +
-                         log(pnorm(-(k[notltiny]-a[notltiny]+t[notltiny]*l[notltiny])/sqr.t)))
-        term.2b <- exp(2*l[notltiny]*(k[notltiny]+a[notltiny]) +
-                         log(pnorm(-(k[notltiny]+a[notltiny]+t[notltiny]*l[notltiny])/sqr.t)))
-        term.2 <- a[notltiny] + (term.2b-term.2a)/(2*l[notltiny])
-
-        term.4a <- 2*pnorm((k[notltiny]+a[notltiny])/sqr.t-sqr.t*l[notltiny])-1
-        term.4b <- 2*pnorm((k[notltiny]-a[notltiny])/sqr.t-sqr.t*l[notltiny])-1
-        term.4c <- .5*(t[notltiny]*l[notltiny] - a[notltiny] - k[notltiny] + .5/l[notltiny])
-        term.4d <- .5*(k[notltiny] - a[notltiny] - t[notltiny]*l[notltiny] - .5/l[notltiny])
-        term.4 <- term.4c*term.4a + term.4d*term.4b
-
-        x[notltiny] <- (term.4 + term.2 + term.1)/(2*a[notltiny])
-      }
-
-      if ( any(ltiny) ) {  # rate zero
-        sqr.t <- sqrt(t[ltiny])
-        log.t <- log(t[ltiny])
-        term.5a <- 2*pnorm((k[ltiny]+a[ltiny])/sqr.t)-1
-        term.5b <- 2*pnorm(-(k[ltiny]-a[ltiny])/sqr.t)-1
-        term.5 <- (-(k[ltiny]+a[ltiny])*term.5a - (k[ltiny]-a[ltiny])*term.5b)/(2*a[ltiny])
-
-        term.6a <- -.5*(k[ltiny]+a[ltiny])^2/t[ltiny] - .5*log(2) -.5*log(pi) + .5*log.t - log(a[ltiny])
-        term.6b <- -.5*(k[ltiny]-a[ltiny])^2/t[ltiny] - .5*log(2) -.5*log(pi) + .5*log.t - log(a[ltiny])
-        term.6 <- 1 + exp(term.6b) - exp(term.6a)
-
-        x[ltiny] <- term.5 + term.6
-      }
-
-    }
-
-    x[x<0 | is.nan(x) ] <- 0
-    x
-  }
-
-  rt <- rt - pars[,"t0"]
   out <- numeric(length(rt))
-  ok <- !pars[,"v"] < 0  # code handles rate zero case
+  ok <- rt > pars[,"t0"] &
+        !pars[,"v"] < 0  # code handles rate zero case
   if (any(dimnames(pars)[[2]]=="s")) # rescale
-    pars[,c("A","B","v")] <- pars[,c("A","B","v")]/pars[,"s"]
-  out[ok] <- pigt(rt[ok],k=pars[ok,"B"]+pars[ok,"A"]/2,l=pars[ok,"v"],a=pars[ok,"A"]/2)
+    pars[ok,c("A","B","v")] <- pars[ok,c("A","B","v")]/pars[ok,"s"]
+  out[ok] <- pWald(rt[ok],v=pars[ok,"v"],B=pars[ok,"B"],A=pars[ok,"A"],t0=pars[ok,"t0"])
   out
-
 }
 
 #### random
@@ -294,6 +296,12 @@ rRDM <- function(lR,pars,p_types=c("v","B","A","t0"))
 
 
 # RDM_B parameterization with s=1 scaling (B = b-A done in rdm.R)
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 rdmB <- function(){
   list(
     type="RACE",
@@ -324,6 +332,12 @@ rdmB <- function(){
 
 
 # RDM_B parameterization with s=1 scaling (B = b-A done in rdm.R)
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 rdmBt0natural <- function(){
   list(
     type="RACE",
