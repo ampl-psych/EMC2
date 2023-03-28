703153baeeca8edc5e8cead03d205d407d10cbef
AJHgit <andrew.heathcote@utas.edu.au>
2023-03-29 00:51:06 +0800

    Enable log_likelihood_race to use NACC factor to deal with differneces between conditions in number of accumulators.

--- R/likelihood.R
+++ R/likelihood.R
@@ -3,27 +3,31 @@ log_likelihood_race <- function(p_vector,dadm,min_ll=log(1e-10))
 {
 
   pars <- get_pars(p_vector,dadm)
+  if (any(names(dadm)=="NACC")) # Some accumulators not present
+    pars[as.numeric(dadm$lR)>dadm$NACC,] <- NA
+
   if (is.null(attr(pars,"ok")))
     ok <- !logical(dim(pars)[1]) else ok <- attr(pars,"ok")
 
-    lds <- numeric(dim(dadm)[1]) # log pdf (winner) or survivor (losers)
-    lds[dadm$winner] <- log(attr(dadm,"model")()$dfun(rt=dadm$rt[dadm$winner],
-                                                      pars=pars[dadm$winner,]))
-    n_acc <- length(levels(dadm$R))
-    if (n_acc>1) lds[!dadm$winner] <- log(1-attr(dadm,"model")()$pfun(rt=dadm$rt[!dadm$winner],pars=pars[!dadm$winner,]))
-    lds[is.na(lds) | !ok] <- min_ll
-    lds <- lds[attr(dadm,"expand")] # decompress
-    if (n_acc>1) {
-      winner <- dadm$winner[attr(dadm,"expand")]
-      ll <- lds[winner]
-      if (n_acc==2) {
-        ll <- ll + lds[!winner]
-      } else {
-        ll <- ll + apply(matrix(lds[!winner],nrow=n_acc-1),2,sum)
-      }
-      ll[is.na(ll)] <- min_ll
-      return(sum(pmax(min_ll,ll)))
-    } else return(sum(pmax(min_ll,lds)))
+  lds <- numeric(dim(dadm)[1]) # log pdf (winner) or survivor (losers)
+  lds[dadm$winner] <- log(attr(dadm,"model")()$dfun(rt=dadm$rt[dadm$winner],
+                                                    pars=pars[dadm$winner,]))
+  n_acc <- length(levels(dadm$R))
+  if (n_acc>1) lds[!dadm$winner] <-
+    log(1-attr(dadm,"model")()$pfun(rt=dadm$rt[!dadm$winner],pars=pars[!dadm$winner,]))
+  lds[is.na(lds) | !ok] <- min_ll
+  lds <- lds[attr(dadm,"expand")] # decompress
+  if (n_acc>1) {
+    winner <- dadm$winner[attr(dadm,"expand")]
+    ll <- lds[winner]
+    if (n_acc==2) {
+      ll <- ll + lds[!winner]
+    } else {
+      ll <- ll + apply(matrix(lds[!winner],nrow=n_acc-1),2,sum)
+    }
+    ll[is.na(ll)] <- min_ll
+    return(sum(pmax(min_ll,ll)))
+  } else return(sum(pmax(min_ll,lds)))
 }
 
 
