61bb0e25dc96f5ee5669f28ea6b995de7828f048
AJHgit <andrew.heathcote@utas.edu.au>
2023-06-21 11:57:51 +0200

    Improved the way compare_CI uses subfilter

--- R/statistics.R
+++ R/statistics.R
@@ -406,16 +406,14 @@ compare_IC <- function(sList,filter="sample",subfilter=0,use_best_fit=TRUE,
     exp(IC)/sum(exp(IC))
   }
 
-  if (length(subfilter)==1) {
-    tmp <- vector(mode="list",length(sList))
-    for (i in 1:length(sList)) tmp[[i]] <- subfilter
-    subfilter=tmp
-  }
-  if ( !is.list(subfilter) || length(subfilter)!=length(sList) )
-    stop("If not a single digit, subfilter must be a list of the same length as sList")
+  if (is.numeric(subfilter)) defaultsf <- subfilter[1] else defaultsf <- 0
+  sflist <- as.list(setNames(rep(defaultsf,length(sList)),names(sList)))
+  if (is.list(subfilter)) for (i in names(subfilter))
+    if (i %in% names(sflist)) sflist[[i]] <- subfilter[[i]]
+
   ICs <- setNames(vector(mode="list",length=length(sList)),names(sList))
   for (i in 1:length(ICs)) ICs[[i]] <- IC(sList[[i]],filter=filter,
-    subfilter=subfilter[[i]],use_best_fit=use_best_fit,subject=subject,print_summary=FALSE)
+    subfilter=sflist[[i]],use_best_fit=use_best_fit,subject=subject,print_summary=FALSE)
   ICs <- data.frame(do.call(rbind,ICs))
   DICp <- getp(ICs$DIC)
   BPICp <- getp(ICs$BPIC)
@@ -433,17 +431,10 @@ compare_IC <- function(sList,filter="sample",subfilter=0,use_best_fit=TRUE,
 compare_ICs <- function(sList,filter="sample",subfilter=0,use_best_fit=TRUE,
                         print_summary=TRUE,digits=3,subject=NULL) {
 
-  if (length(subfilter)==1) {
-    tmp <- vector(mode="list",length(sList))
-    for (i in 1:length(sList)) tmp[[i]] <- subfilter
-    subfilter=tmp
-  }
-  if ( !is.list(subfilter) || length(subfilter)!=length(sList) )
-    stop("If not a single digit, subfilter must be a list of the same length as sList")
   subjects <- names(sList[[1]][[1]]$data)
   out <- setNames(vector(mode="list",length=length(subjects)),subjects)
   for (i in subjects) out[[i]] <- compare_IC(sList,subject=i,
-                                             filter=filter,subfilter=subfilter,use_best_fit=use_best_fit,print_summary=FALSE)
+    filter=filter,subfilter=subfilter,use_best_fit=use_best_fit,print_summary=FALSE)
   if (print_summary) {
     wDIC <- lapply(out,function(x)x["wDIC"])
     wBPIC <- lapply(out,function(x)x["wBPIC"])
