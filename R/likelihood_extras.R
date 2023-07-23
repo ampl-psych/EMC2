173465b10430799ddbb73d4a382d18e32e9392a2
AJHgit <andrew.heathcote@utas.edu.au>
2023-07-23 12:50:00 +0200

    robustness fixes for SS likelihood

--- R/likelihood_extras.R
+++ R/likelihood_extras.R
@@ -153,8 +153,6 @@ log_likelihood_race_ss <- function(p_vector,dadm,min_ll=log(1e-10))
 
   if (is.null(attr(pars,"ok")))
     ok <- !logical(dim(pars)[1]) else ok <- attr(pars,"ok")
-  ok <- ok[attr(dadm,"expand")][dadm$winner[attr(dadm,"expand")]]
-  if (all(!ok)) return(min_ll*length(ok))
 
   # standard race, ignores R=NA
   isstop <- dadm[,"lR"]=="stop"
@@ -170,7 +168,7 @@ log_likelihood_race_ss <- function(p_vector,dadm,min_ll=log(1e-10))
     lds[golooser] <- log(1-attr(dadm,"model")()$pfunG(rt=dadm$rt[golooser],pars=pars[golooser,,drop=FALSE]))
     lds[stoplooser] <- log(1-attr(dadm,"model")()$pfunS(rt=dadm$rt[stoplooser],pars=pars[stoplooser,,drop=FALSE]))
   }
-  lds[is.na(lds)] <- 0
+  lds[is.na(lds) | !ok] <- 0
   lds <- lds[attr(dadm,"expand")] # decompress
   winner <- dadm$winner[attr(dadm,"expand")]
   if (n_acc>1) {
@@ -178,7 +176,7 @@ log_likelihood_race_ss <- function(p_vector,dadm,min_ll=log(1e-10))
     if (n_acc==2)
       ll <- ll + lds[!winner] else
         ll <- ll + apply(matrix(lds[!winner],nrow=n_acc-1),2,sum)
-      ll[is.na(ll)] <- 0
+    ll[is.na(ll)] <- 0
   } else ll <- lds
   like <- exp(ll)
 
@@ -209,8 +207,7 @@ log_likelihood_race_ss <- function(p_vector,dadm,min_ll=log(1e-10))
     like[!goresp] <- gf[!goresp] + (1-gf[!goresp])*like[!goresp]
     like[goresp] <- like[goresp]*(1-gf[goresp])
   }
-  like[like<0 | is.na(like) | !ok] <- 0
-
+  like[like<0 | is.na(like)] <- 0
   sum(pmax(min_ll,log(like)))
 }
 
