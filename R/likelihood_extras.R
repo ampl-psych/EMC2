dada2968252aee3c71f10e2a58bab487c11a9b8d
AJHgit <andrew.heathcote@utas.edu.au>
2023-07-26 13:14:12 +0200

    Extra protection in log_likelihood_race_ss

--- R/likelihood_extras.R
+++ R/likelihood_extras.R
@@ -208,7 +208,7 @@ log_likelihood_race_ss <- function(p_vector,dadm,min_ll=log(1e-10))
     like[goresp] <- like[goresp]*(1-gf[goresp])
   }
   like[like<0 | is.na(like)] <- 0
-  sum(pmax(min_ll,log(like)))
+  if (all(is.numeric(like))) sum(pmax(min_ll,log(like))) else min_ll
 }
 
 
