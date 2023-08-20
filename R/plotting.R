bce98f4ce7c7d3a894318fc347db9bd6c9b04166
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2023-08-20 10:41:38 +0200

    Change plot_density to plot_pars

--- R/plotting.R
+++ R/plotting.R
@@ -103,11 +103,11 @@ plot_acfs <- function(samples,layout=NULL,subject=1,
   }
 }
 
-#' Uses output from plot_density to plot true vs. estimated (median with CI) alpha
+#' Uses output from plot_pars to plot true vs. estimated (median with CI) alpha
 #' parameters for each subject (plot density must be called with true values passed
 #' through the pars argument).
 #'
-#' @param tabs Tables of actual and estimated alpha parameters (with CIs) from plot_density
+#' @param tabs Tables of actual and estimated alpha parameters (with CIs) from plot_pars
 #' @param layout A 2-vector specifying the layout as in par(mfrow = layout)
 #' @param do_ci Boolean (Default TRUE). Add CIs to plot?
 #' @param ci_col Color of CI.
@@ -132,7 +132,7 @@ plot_alpha_recovery <- function(tabs,layout=c(2,3),
                                 rmse_digits=3,pearson_digits=2,
                                 do_coverage=TRUE,coverage_pos="bottomright",
                                 coverage_digits=1,spearman_digits=2)
-  # Takes tables output by plot_density with par and plots recovery
+  # Takes tables output by plot_pars with par and plots recovery
 {
   par(mfrow=layout)
   pnams <- dimnames(tabs[[1]])[[2]]
@@ -294,7 +294,7 @@ plot_defective_density <- function(data,subject=NULL,factors=NULL,
 #' @param add_means Boolean (default FALSE) add parameter means as an attribute
 #' to return
 #' @param pars Named vector or matrix of true parameters, or the output of
-#' plot_density, in which case the posterior medians are extracted.
+#' plot_pars, in which case the posterior medians are extracted.
 #' @param probs Vector (default c(.025,.5,.975)) for CI and central tendency of return
 #' @param bw Bandwidth for density plot (see density)
 #' @param adjust Adjustment for density plot (see density)
@@ -307,7 +307,7 @@ plot_defective_density <- function(data,subject=NULL,factors=NULL,
 #'
 #' @export
 
-plot_density <- function(pmwg_mcmc,layout=c(2,3),
+plot_pars <- function(pmwg_mcmc,layout=c(2,3),
   selection="alpha",filter="sample",thin=1,subfilter=0,mapped=FALSE,
   plot_prior=TRUE,n_prior=1e3,xlim=NULL,ylim=NULL,prior_xlim=NULL,
   show_chains=FALSE,do_plot=TRUE,subject=NA,add_means=FALSE,
