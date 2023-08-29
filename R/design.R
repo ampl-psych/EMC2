477d68ce28fd00317643c689e575563100ff29cf
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2023-08-29 17:57:05 +0200

    Add checks on prior to design_model

--- R/design.R
+++ R/design.R
@@ -474,21 +474,34 @@ design_model <- function(data,design,model=NULL,prior = NULL,
   # Pick out constants
   sampled_p_names <- p_names[!(p_names %in% names(design$constants))]
 
-  # if(!is.null(prior)){
-  #   if(length(prior$theta_mu_mean) != length(sampled_p_names))
-  #     stop("prior mu should be same length as estimated parameters (p_vector)")
-  #   if(!is.matrix(prior$theta_mu_var)){
-  #     if(length(prior$theta_mu_var) != length(sampled_p_names))
-  #       stop("prior theta should be same length as estimated parameters (p_vector)")
-  #   } else {
-  #     if(nrow(prior$theta_mu_var) != length(sampled_p_names))
-  #       stop("prior theta should have same number of rows as estimated parameters (p_vector)")
-  #     if(ncol(prior$theta_mu_var) != length(sampled_p_names))
-  #       stop("prior theta should have same number of columns as estimated parameters (p_vector)")
-  #     if(ncol(prior$theta_mu_var) != nrow(prior$theta_mu_var))
-  #       stop("prior theta should have same number of columns as rows")
-  #   }
-  # }
+  if(!is.null(prior)) {
+    if(length(prior$theta_mu_mean) != length(sampled_p_names))
+      stop("prior mu should be same length as estimated parameters (p_vector)")
+    if (is.null(names(prior$theta_mu_mean)))
+      stop("theta_mu_mean must have names")
+    if (!all(sort(names(prior$theta_mu_mean)) == sort(sampled_p_names)))
+      stop("theta_mu_mean names not the same as sampled paramter names")
+    # Make sure theta_mu_mean has same order as sampled parameters
+    pnams <- names(prior$theta_mu_mean)
+    prior$theta_mu_mean <- prior$theta_mu_mean[sampled_p_names]
+    if(!is.matrix(prior$theta_mu_var)) {
+      if(length(prior$theta_mu_var) != length(sampled_p_names))
+        stop("prior theta should be same length as estimated parameters (p_vector)")
+      # Make sure theta_mu_var has same order as sampled parameters
+      names(prior$theta_mu_var) <- pnams
+      prior$theta_mu_var <- prior$theta_mu_var[sampled_p_names]
+    } else {
+      if(nrow(prior$theta_mu_var) != length(sampled_p_names))
+        stop("prior theta should have same number of rows as estimated parameters (p_vector)")
+      if(ncol(prior$theta_mu_var) != length(sampled_p_names))
+        stop("prior theta should have same number of columns as estimated parameters (p_vector)")
+      # Make sure theta_mu_var has same order as sampled parameters
+      dimnames(prior$theta_mu_var) <- list(pnams,pnams)
+      prior$theta_mu_var <- prior$theta_mu_var[sampled_p_names,sampled_p_names]
+    }
+
+  }
+
   attr(dadm, "prior") <- prior
   attr(dadm,"p_names") <- p_names
   attr(dadm,"model") <- model
