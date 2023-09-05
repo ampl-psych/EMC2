46f13ac24ca2611fc49d2cefd0bee3a2ac4e3c7d
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2023-09-05 17:41:35 +0200

    Make MlbaB available

--- R/model_LBA.R
+++ R/model_LBA.R
@@ -279,37 +279,36 @@ albaB <- function(){
     }
   )}
 
-# lba_B parameterization with sv=1 scaling
-# MlbaB <- function(){
-#   list(
-#     type="RACE",
-#     p_types=c("v","sv","B","A","t0"),
-#     Ntransform=function(x) {
-#       # Transform to natural scale
-#       x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
-#       x
-#     },
-#     # p_vector transform, sets sv as a scaling parameter
-#     transform = function(p) p,
-#     # Trial dependent parameter transform
-#     Ttransform = function(pars,dadm) {
-#       pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
-#       attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
-#       pars
-#     },
-#     # Random function for racing accumulator
-#     Random function for racing accumulator
-#     rfun=function(lR=NULL,pars) {
-#       ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
-#       if (is.null(lR)) ok else rLBA(lR,pars,posdrift=TRUE)
-#     },
-#     # Density function (PDF) for single accumulator
-#     dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
-#     # Probability function (CDF) for single accumulator
-#     pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
-#     # Race likelihood combining pfun and dfun
-#     log_likelihood=function(p_vector,dadm,min_ll=log(1e-10)){
-#       log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
-#     }
-#   )
-# }
+# Handles missing values
+MlbaB <- function(){
+  list(
+    type="RACE",
+    # p_vector transform, sets sv as a scaling parameter
+    p_types=c("v","sv","B","A","t0"),
+    transform = function(p) p,
+    # Transform to natural scale
+    Ntransform=function(x) {
+      x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
+      x
+    },
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) {
+      pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      pars
+    },
+    # Random function for racing accumulator
+    rfun=function(lR=NULL,pars) {
+      ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      if (is.null(lR)) ok else rLBA(lR,pars,posdrift=TRUE,ok=ok)
+    },
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10)){
+      log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+    }
+  )
+}
