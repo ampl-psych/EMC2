6fa6424f8bca5447718316e9e6d9a020caf0b4ea
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2023-09-15 11:28:39 +0200

    Updated dLBA and pLBA to deal with infinite dt, added MlbaB model

--- R/model_LBA.R
+++ R/model_LBA.R
@@ -102,7 +102,7 @@ dLBA <- function (rt, pars, posdrift = TRUE, robust = FALSE)
 {
   dt <- rt - pars[,"t0"]
   ok <- (dt>0) & (pars[,"b"] >= pars[,"A"])
-  ok[is.na(ok)] <- FALSE
+  ok[is.na(ok) | !is.finite(dt)] <- FALSE
   out <- numeric(length(dt))
   out[ok] <- dlba(t = dt[ok], A = pars[ok,"A"], b = pars[ok,"b"],
                          v = pars[ok,"v"], sv = pars[ok,"sv"],
@@ -116,7 +116,7 @@ pLBA <- function (rt, pars, posdrift = TRUE, robust = FALSE)
 {
   dt <- rt - pars[,"t0"]
   ok <- (dt>0) & (pars[,"b"] >= pars[,"A"])
-  ok[is.na(ok)] <- FALSE
+  ok[is.na(ok) | !is.finite(dt)] <- FALSE
   out <- numeric(length(dt))
   out[ok] <- plba(t = dt[ok], A = pars[ok,"A"], b = pars[ok,"b"],
                          v = pars[ok,"v"], sv = pars[ok,"sv"],
@@ -165,7 +165,7 @@ rLBA <- function(lR,pars,p_types=c("v","sv","b","A","t0"),posdrift = TRUE,
 #'
 #' Here we use the b = B + A parameterization, which ensures that the response threshold is always higher than the between trial variation in start point of the drift rate.
 #'
-#' @return A list defining the cognitive model
+#' @return A model list with all the necessary functions to sample
 #' @export
 
 lbaB <- function(){
@@ -202,11 +202,46 @@ lbaB <- function(){
   )
 }
 
-# lba_B parameterization
-#' Title
+
+#' lba_B Model accommodating missing values (truncation and censoring)
 #'
-#' @return A list defining the cognitive model
+#' @return A model list with all the necessary functions to sample
 #' @export
+#'
+#' @examples
+MlbaB <- function(){
+  list(
+    type="RACE",
+    p_types=c("v","sv","B","A","t0"),
+    Ntransform=function(x) {
+      # Transform to natural scale
+      x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
+      x
+    },
+    # p_vector transform
+    transform = function(p) p,
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) {
+      pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      pars
+    },
+    # # Random function for racing accumulator
+    # Random function for racing accumulator
+    rfun=function(lR=NULL,pars) {
+      ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      if (is.null(lR)) ok else rLBA(lR,pars,posdrift=TRUE)
+    },
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm){
+      log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll=log(1e-10))
+    }
+  )
+}
 
 albaB <- function(){
   list(
@@ -242,37 +277,3 @@ albaB <- function(){
     }
   )}
 
-# lba_B parameterization with sv=1 scaling
-# MlbaB <- function(){
-#   list(
-#     type="RACE",
-#     p_types=c("v","sv","B","A","t0"),
-#     Ntransform=function(x) {
-#       # Transform to natural scale
-#       x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
-#       x
-#     },
-#     # p_vector transform, sets sv as a scaling parameter
-#     transform = function(p) p,
-#     # Trial dependent parameter transform
-#     Ttransform = function(pars,dadm) {
-#       pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
-#       attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
-#       pars
-#     },
-#     # Random function for racing accumulator
-#     Random function for racing accumulator
-#     rfun=function(lR=NULL,pars) {
-#       ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
-#       if (is.null(lR)) ok else rLBA(lR,pars,posdrift=TRUE)
-#     },
-#     # Density function (PDF) for single accumulator
-#     dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
-#     # Probability function (CDF) for single accumulator
-#     pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
-#     # Race likelihood combining pfun and dfun
-#     log_likelihood=function(p_vector,dadm,min_ll=log(1e-10)){
-#       log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
-#     }
-#   )
-# }
