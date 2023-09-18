bd30ff0576be9da1e3c4d735b13cd87e556c89c7
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2023-09-18 16:00:51 +0200

    Add MTlbaB and TClbaB models

--- R/model_LBA.R
+++ R/model_LBA.R
@@ -221,7 +221,7 @@ MlbaB <- function(){
     # Trial dependent parameter transform
     Ttransform = function(pars,dadm) {
       pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
-      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | (pars[,"A"] == 0))
       pars
     },
     # # Random function for racing accumulator
@@ -240,6 +240,85 @@ MlbaB <- function(){
     }
   )
 }
+
+
+#' Multiple Threshold LBA model
+#'
+#' @return A model list with all the necessary functions to sample
+#' @export
+MTlbaB <- function(){
+  list(
+    type="MT",
+    # p_vector transform, sets sv as a scaling parameter
+    p_types=c("v","sv","B","A","t0"),
+    transform = function(p) p,
+    # Transform to natural scale
+    Ntransform=function(x) {
+      x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
+      x
+    },
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) {
+      pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
+
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      pars
+    },
+    # Random function for racing accumulator
+    rfun=function(lR=NULL,pars) {
+      ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      if (is.null(lR)) ok else rLBA(lR,pars,posdrift=TRUE,ok=ok)
+    },
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10)){
+      log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+    }
+  )
+}
+
+#' Threshold Count LBA model
+#'
+#' @return A model list with all the necessary functions to sample
+#' @export
+TClbaB <- function(){
+  list(
+    type="TC",
+    # p_vector transform, sets sv as a scaling parameter
+    p_types=c("v","sv","B","A","t0"),
+    transform = function(p) p,
+    # Transform to natural scale
+    Ntransform=function(x) {
+      x[,dimnames(x)[[2]] != "v"] <- exp(x[,dimnames(x)[[2]] != "v"])
+      x
+    },
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) {
+      pars <- cbind(pars,b=pars[,"B"] + pars[,"A"])
+
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      pars
+    },
+    # Random function for racing accumulator
+    rfun=function(lR=NULL,pars) {
+      ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      if (is.null(lR)) ok else rLBA(lR,pars,posdrift=TRUE,ok=ok)
+    },
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLBA(rt,pars,posdrift = TRUE, robust = FALSE),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10)){
+      log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+    }
+  )
+}
+
+
 
 albaB <- function(){
   list(
