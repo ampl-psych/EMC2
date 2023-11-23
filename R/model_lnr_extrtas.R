e573def7f0c85025d6a6567ee8d1fe8e9be8abc7
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2023-11-23 14:59:29 +0100

    Added s proporitonal to m model to lnr

--- R/model_lnr_extrtas.R
+++ R/model_lnr_extrtas.R
@@ -32,3 +32,40 @@ MlnrMS <- function(){
       log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
   )
 }
+
+
+#' lnr model where s proportional to m
+#'
+#' @return
+#' @export
+#'
+#' @examples
+lnrMP <- function() {
+  list(
+    type="RACE",
+    c_name = "lnrMS",
+    p_types=c("m","s","t0","sp"),
+    Ntransform=function(x) {
+      # Transform to natural scale
+      x[,dimnames(x)[[2]] != "m"] <- exp(x[,dimnames(x)[[2]] != "m"])
+      x[,"s"] <- x[,"s"] + exp(x[,"m"])*x[,"sp"]
+      x
+    },
+    # p_vector transform scaling parameter by s=1 assumed in lnr.R
+    transform = function(x) x,
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) pars,
+    # Random function for racing accumulators
+    rfun=function(lR=NULL,pars) {
+      if (is.null(lR)) return(rep(TRUE,dim(pars)[1]))
+      rLNR(lR,pars)
+    },
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLNR(rt,pars),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLNR(rt,pars),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10))
+      log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+  )
+}
