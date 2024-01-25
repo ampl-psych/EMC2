262f16d632e216b6abe33dc8551f4d135c73a033
Niek Stevenson <niek.stevenson@gmail.com>
2024-01-25 12:02:42 +0100

    added back return value to bridge sampling...

--- R/bridge_sampling.R
+++ R/bridge_sampling.R
@@ -185,7 +185,7 @@ bridge_sampling <- function(samples, n_eff, split_idx, cores_for_props = 1, core
   if(is.na(tmp$logml)){
     stop("Bridge sampling did not converge, usually this means you need to run_emc longer to get more samples")
   }
-
+  return(tmp$logml)
 }
 
 
