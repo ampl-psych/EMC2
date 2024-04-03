632912ed8267b45c5824bf397de11165f1add6c4
Michelle Donzallaz <michelle.donzallaz@ggaweb.ch>
2024-04-03 17:15:03 +0200

    help file review for standard

--- R/variant_standard.R
+++ R/variant_standard.R
@@ -27,17 +27,21 @@ add_info_standard <- function(sampler, prior = NULL, ...){
 #' For details see Huang, A., & Wand, M. P. (2013). Simple marginally noninformative
 #' prior distributions for covariance matrices. *Bayesian Analysis*, 8, 439-452. https://doi.org/10.1214/13-BA815
 #'
-#' @param prior A named list that can contain the prior mean (theta_mu_mean) and
-#' variance (theta_mu_var) on the group-level mean, or the scale (A), or degrees of freedom (df)
-#' for the group-level variance-covariance matrix.
-#' Default prior created for NULL entries.
-#' @param n_pars Often inferred from the design, but if design = NULL will be used to determine size of prior.
-#' @param sample Whether to sample from the prior or to simply return the prior. Default is TRUE,
-#' @param map Boolean, default TRUE reverses malformation used by model to make
-#' sampled parameters unbounded
-#' @param N How many samples to draw from the prior, default 1e5
-#' @param design The design obtained from `make_design`, required when map = TRUE
-#' @param type character. If sample = TRUE, what prior to sample from. Options: "mu", "variance", "covariance" "full_var", "alpha".
+#' @param prior A named list that can contain the prior mean (`theta_mu_mean`) and
+#' variance (`theta_mu_var`) on the group-level mean, or the scale (`A`), or degrees of freedom (`df`)
+#' for the group-level variance-covariance matrix. For `NULL` entries, a default prior gets created.
+#' @param n_pars Often inferred from the design, but if `design = NULL`, `n_pars`
+#' will be used to determine the size of prior.
+#' @param sample Boolean, defaults to `TRUE`, sample from the prior or simply return the prior specifications?
+#' @param map Boolean, defaults to `TRUE`. If `sample = TRUE`, the implied prior is sampled.
+#' This includes back-transformations for naturally bounded parameters such as
+#' the non-decision time and an inverse mapping from the design matrix back to the
+#' cells of the design. If `FALSE`, the transformed, unmapped, parameters are used.
+#' Note that `map` does not affect the prior used in the sampling process.
+#' @param N How many samples to draw from the prior, the default is 1e5
+#' @param design The design obtained from `make_design()`, required when `map = TRUE`
+#' @param type Character. If `sample = TRUE`, what prior to sample from. Options:
+#' `"mu"`, `"variance"`, `"covariance"` `"full_var"`, `"alpha"`.
 #'
 #' @return A list with a single entry of type of samples from the prior (if sample = TRUE) or else a prior object
 #' @examples \dontrun{
