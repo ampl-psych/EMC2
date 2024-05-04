1225c318de4eb7cdbcbc44a2d2239fb45d7786a0
Niek Stevenson <niek.stevenson@gmail.com>
2024-05-04 17:07:27 +0200

    updated make_design and make_samplers with str() and give.attr = FALSE to circumvent environment issue for now

--- tests/testthat/test-make_design.R
+++ tests/testthat/test-make_design.R
@@ -1,12 +1,14 @@
+design <- make_design(data = data.frame(forstmann, CO = 1:nrow(forstmann)),
+            model=LBA,matchfun=function(d)d$S==d$lR,
+            formula=list(v~lM,sv~lM,B~E+lR,t0~E2 + CO),
+            contrasts=list(v = list(lM=matrix(c(-1/2,1/2),ncol=1,dimnames=list(NULL,"d")))),
+            constants=c(sv=log(1)),
+            functions = list(
+              E2 = function(d) factor(d$E!="speed",labels=c("speed","nonspeed"))
+            ))
+
 test_that("make_design", {
   expect_snapshot(
-    design <- make_design(data = data.frame(forstmann, CO = 1:nrow(forstmann)),
-                model=LBA,matchfun=function(d)d$S==d$lR,
-                formula=list(v~lM,sv~lM,B~E+lR,t0~E2 + CO),
-                contrasts=list(v = list(lM=matrix(c(-1/2,1/2),ncol=1,dimnames=list(NULL,"d")))),
-                constants=c(sv=log(1)),
-                functions = list(
-                  E2 = function(d) factor(d$E!="speed",labels=c("speed","nonspeed"))
-                ))
+    str(design, give.attr = FALSE)
   )
 })
---
+++ tests/testthat/test-make_samplers.R
@@ -0,0 +1,20 @@
+matchfun <- function(d)d$S==d$lR
+# design an "average and difference" contrast matrix
+ADmat <- matrix(c(-1/2,1/2),ncol=1,dimnames=list(NULL,"diff"))
+
+# specify design
+design_LBABE <- make_design(data = forstmann,model=LBA,
+                formula=list(v~1,sv~1,B~E+lR,A~1,t0~1),
+                constants=c(sv=log(1)))
+
+pmean <- c(v=1, B=log(.5),B_Eneutral=log(1.5),
+           B_Eaccuracy=log(2),B_lRright=0, A=log(0.25),t0=log(.2))
+psd <- c(v=1,
+         B=0.3,B_Eneutral=0.3,B_Eaccuracy=0.3,B_lRright=0.3,A=0.4,t0=.5)
+prior_LBABE <- make_prior(design_LBABE, type = 'standard',pmean=pmean,psd=psd)
+samplers <- make_samplers(forstmann,design_LBABE,type="standard",  prior=prior_LBABE,
+                          compress = FALSE)
+
+test_that("make_samplers", {
+  expect_snapshot(str(samplers, give.attr = F))
+})
