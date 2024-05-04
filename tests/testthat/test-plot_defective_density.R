fb0f8af45cfa9a10e95f42d3e599e7de5c817aab
Niek Stevenson <niek.stevenson@gmail.com>
2024-05-04 11:52:30 +0200

    updated test files with image for plot_defective_density

--- tests/testthat/test-plot_defective_density.R
+++ tests/testthat/test-plot_defective_density.R
@@ -1,7 +1,6 @@
-test_full <- plot_defective_density(forstmann, factors = c("E"),
-                               correct_fun = function(d) d$R == d$S)
-
-test_subject <- plot_defective_density(forstmann, factors = c("E", "S"), subject = 1,
-                               mfcol = TRUE, layout = c(2,3))
-expect_doppelganger("density_full", test_full)
-expect_doppelganger("density_subject", test_subject)
+expect_doppelganger("density_subject",
+                    plot_defective_density(forstmann, factors = c("E", "S"), subject = 1,
+                                           mfcol = TRUE, layout = c(2,3)))
+expect_doppelganger("density_full",
+                    plot_defective_density(forstmann, factors = c("E", "S"),
+                                           correct_fun = function(d) d$R == d$S))
