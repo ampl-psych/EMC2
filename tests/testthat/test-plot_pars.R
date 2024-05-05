c28656014a2bc8e173af28e9807d398412ea2da2
Niek Stevenson <niek.stevenson@gmail.com>
2024-05-05 14:56:18 +0200

    added appropriate seed type to plot_pars

--- tests/testthat/test-plot_pars.R
+++ tests/testthat/test-plot_pars.R
@@ -1,4 +1,5 @@
 test_that("plot_pars", {
+  RNGkind("L'Ecuyer-CMRG")
   set.seed(123)
   vdiffr::expect_doppelganger("density plots", plot_pars(samplers_LNR))
   expect_snapshot(plot_pars(samplers_LNR, selection = "variance", do_plot = FALSE))
