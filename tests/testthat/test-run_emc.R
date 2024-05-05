22d4a79ac20fb760c9d3b6b156198bea609a5149
Niek Stevenson <niek.stevenson@gmail.com>
2024-05-05 14:18:10 +0200

    small updates to run_emc and model_function tests

--- tests/testthat/test-run_emc.R
+++ tests/testthat/test-run_emc.R
@@ -5,20 +5,30 @@ ADmat <- matrix(c(-1/2,1/2),ncol=1,dimnames=list(NULL,"d"))
 matchfun=function(d)d$S==d$lR
 
 # Drop most subjects
-dat <- forstmann[forstmann$subjects %in% unique(forstmann$subjects)[1:3],]
+dat <- forstmann[forstmann$subjects %in% unique(forstmann$subjects)[1:2],]
 dat$subjects <- droplevels(dat$subjects)
 
 design_LNR <- make_design(data = dat,model=LNR,matchfun=matchfun,
                             formula=list(m~lM,s~1,t0~1),
                             contrasts=list(m=list(lM=ADmat)))
 
-LNR_s <- make_samplers(dat, design_LNR, rt_resolution = 0.05)
+LNR_s <- make_samplers(dat, design_LNR, rt_resolution = 0.02, n_chains = 2)
+
 RNGkind("L'Ecuyer-CMRG")
 set.seed(123)
-LNR_s <- run_emc(LNR_s, cores_per_chain = 3, stop_criteria = list(
-  preburn = list(iter = 100), adapt = list(min_unique = 100)
-))
+LNR_s <- run_emc(LNR_s, cores_per_chain = 2, cores_for_chains = 2, stop_criteria = list(
+  preburn = list(iter = 100), adapt = list(min_unique = 100),
+  sample = list(iter = 25)), verbose = FALSE)
+idx <- LNR_s[[1]]$samples$idx
 
 test_that("run_emc", {
-  expect_equal(2 * 2, 4)
+  expect_snapshot(
+    LNR_s[[1]]$samples$theta_mu[,idx]
+  )
+  expect_snapshot(
+    LNR_s[[1]]$samples$alpha[,,idx]
+  )
+  expect_snapshot(
+    LNR_s[[1]]$samples$theta_var[,,idx]
+  )
 })
