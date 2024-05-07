cb040c558adae0c92df2bea9be2b033c4ab33304
Niek Stevenson <niek.stevenson@gmail.com>
2024-05-07 11:20:03 +0200

    More realistic value for savage_dickey which circumvented OS differences in handling extreme value

--- tests/testthat/test-compare.R
+++ tests/testthat/test-compare.R
@@ -1,16 +1,15 @@
+RNGkind("L'Ecuyer-CMRG")
+set.seed(123)
+
 test_that("compare", {
-  RNGkind("L'Ecuyer-CMRG")
-  set.seed(123)
   expect_snapshot(
     compare(list(samplers_LNR))
   )
 })
 
 test_that("savage-dickey", {
-  RNGkind("L'Ecuyer-CMRG")
-  set.seed(123)
   expect_snapshot(
-    savage_dickey(samplers_LNR, parameter = "m", do_plot = F))
+    savage_dickey(samplers_LNR, parameter = "m", do_plot = F, H0 = -1))
   expect_snapshot(
     savage_dickey(samplers_LNR, fun = function(d) d["m"] - d["m_lMd"],
                   H0 = -0.5, do_plot = F))
