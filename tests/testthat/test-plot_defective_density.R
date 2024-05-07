e72203bbc6193a5a7d61378f105be4ef049ed1fd
Niek Stevenson <niek.stevenson@gmail.com>
2024-05-07 11:05:15 +0200

    made plot_defective_density more comprehensive

--- tests/testthat/test-plot_defective_density.R
+++ tests/testthat/test-plot_defective_density.R
@@ -2,9 +2,14 @@ test_that("plot_defective_density", {
     vdiffr::expect_doppelganger("density_subject",
                       plot_defective_density(forstmann, factors = c("E", "S"), subject = 1,
                                              mfcol = TRUE, layout = c(2,3)))
-    vdiffr::expect_doppelganger("density_full",
-                      plot_defective_density(forstmann, factors = c("E", "S"),
-                                             correct_fun = function(d) d$R == d$S))
+    vdiffr::expect_doppelganger("density_full_E",
+                      plot_defective_density(forstmann, factors = c("E"),
+                                             correct_fun = function(d) d$R == d$S,
+                      layout = c(3,1)))
+    vdiffr::expect_doppelganger("density_full_S",
+                                plot_defective_density(forstmann, factors = c("S"),
+                                                       correct_fun = function(d) d$R == d$S,
+                                                       layout = c(1,3)))
   }
 )
 
