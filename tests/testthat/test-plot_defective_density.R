20fbfad11b4ce063986974a637307132348528e8
Niek Stevenson <niek.stevenson@gmail.com>
2024-05-26 17:25:52 +0200

    fixed more updates in test files

--- tests/testthat/test-plot_defective_density.R
+++ tests/testthat/test-plot_defective_density.R
@@ -1,7 +1,7 @@
 test_that("density_plot", {
     vdiffr::expect_doppelganger(paste0("density_subject_", Sys.info()[1], Sys.info()[2]),
                       plot_defective_density(forstmann, factors = c("E", "S"), subject = 1,
-                                             mfcol = TRUE, layout = c(2,3)))
+                                             layout = c(2,3)))
     vdiffr::expect_doppelganger(paste0("density_full_E_", Sys.info()[1], Sys.info()[2]),
                       plot_defective_density(forstmann, factors = c("E"),
                                              correct_fun = function(d) d$R == d$S,
