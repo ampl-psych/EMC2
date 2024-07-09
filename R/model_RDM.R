95de8a9e72abfecf0dc0975aea528f4749b16d4a
Niek Stevenson <niek.stevenson@gmail.com>
2024-07-09 13:03:18 +0200

    removed RDMt0natural

--- R/model_RDM.R
+++ R/model_RDM.R
@@ -404,37 +404,3 @@ RDM <- function(){
       log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
   )
 }
-
-
-RDMt0natural <- function(){
-  list(
-    type="RACE",
-    p_types=c("v" = log(1),"B" = log(1),"A" = log(0),"t0" = 0,"s" = log(1)),
-    # Transform to natural scale
-    Ntransform=function(x) {
-      x[,dimnames(x)[[2]]  != "t0"] <- exp(x[,dimnames(x)[[2]]  != "t0"])
-      x
-    },
-    # p_vector transform
-    transform = function(x) x,
-    # Trial dependent parameter transform
-    Ttransform = function(pars,dadm) {
-      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0) &
-        ((pars[,"v"] > 1e-3) | pars[,"v"] == 0)
-      pars
-    },
-    # Random function for racing accumulators
-    rfun=function(lR=NULL,pars) {
-      ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0) &
-        ((pars[,"v"] > 1e-3) | pars[,"v"] == 0)
-      if (is.null(lR)) ok else rRDM(lR,pars,ok=ok)
-    },
-    # Density function (PDF) for single accumulator
-    dfun=function(rt,pars) dRDM(rt,pars),
-    # Probability function (CDF) for single accumulator
-    pfun=function(rt,pars) pRDM(rt,pars),
-    # Race likelihood combining pfun and dfun
-    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10))
-      log_likelihood_race(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
-  )
-}
