3b3a0fb11753c37825a5b01b762a7b16bf1998b3
Niek Stevenson <niek.stevenson@gmail.com>
2024-07-09 13:03:52 +0200

    Made sure right stage was ran after subsetting samples in fit()

--- R/s3_funcs.R
+++ R/s3_funcs.R
@@ -355,15 +355,8 @@ fit.emc <- function(emc, stage = NULL, iter = 1000, stop_criteria = NULL,report_
   }
   if(is.null(stage)){
     nstage <- colSums(chain_n(emc))
-    if (nstage["preburn"]==0) {
-      stage <- "preburn"
-    } else{
-      if (nstage["sample"]>0){
-        stage <- "sample"
-      } else{
-        stage <- names(nstage)[which(nstage==0)[1] - 1]
-      }
-    }
+    has_ran <- nstage[nstage != 0]
+    stage <- names(has_ran)[length(has_ran)]
   }
   if(stage == "preburn"){
     emc <- run_emc(emc, stage = "preburn", stop_criteria[['preburn']], cores_for_chains = cores_for_chains, p_accept = p_accept,
