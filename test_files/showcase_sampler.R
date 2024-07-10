2cc013c9a8f939bdf25e89e8671c8b8d3fc0ec39
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2024-07-11 06:22:15 +1000

    A few edits to make more formal

--- test_files/showcase_sampler.R
+++ test_files/showcase_sampler.R
@@ -30,36 +30,49 @@ library(EMC2)
 # posterior_summary
 # profile_plot
 # subset
-# get_pars #BACKBONE OF ALL (the used in ll version is now called get_pars_matrix)
+# get_pars #BACKBONE OF ALL (the version used in ll version is now called
+# get_pars_matrix)
 
 # Got rid of:
 # attributes model_list and data_list and a whole lot of backend functions/code
 
 ############# IMPORTANT ########################
-#### Almost every argument of get_pars works for every new function, and almost all can be arbitrarily combined ###
+#### Almost every argument of get_pars works for every new function, and almost
+#### all can be arbitrarily combined ###
 
 # OK let's get going:
-# It pays off to look thoroughly at ?get_pars, defaults are mentioned in the function description
+# It pays off to look thoroughly at ?get_pars, defaults are mentioned in the
+# function description
 ?get_pars
-# let's go over each option separately
-# These samples only contain 50 ''sample'' stage samples, so can't set different stages
+
+# let's go over each option separately focusing on those relevant for plotting.
+# The type argument is only for other functions, and so is true_pars
+# the get_pars return_mcmc argument is more for other functions and not
+# applicable for plot, when TRUE it returns an mcmc object
+
+# We will use a small built in emc object that only contains 50 ''sample''
+# stage samples, so can't set differently here
 plot(samples_LNR, selection = "mu", stage = "adapt")
-# But I can subset
+
+# Samples can be sub-setted in three ways
 plot(samples_LNR, selection = "mu", filter = 20) # will exclude the first 20
 plot(samples_LNR, selection = "mu", filter = c(1,2,3,20:30)) # only takes these
-# I can alo thin
-plot(samples_LNR, selection = "mu", thin = 2)
-# Or I can set a lenght.out
 plot(samples_LNR, selection = "mu", length.out = 20)
+
+# They can also be thinned
+plot(samples_LNR, selection = "mu", thin = 2)
+
 # Use map = TRUE to map
 plot(samples_LNR, selection = "mu", map = TRUE)
-# The by subject argument determines how the plots/stats are presented for selection = 'alpha'
+
+# The by subject argument determines how the plots/stats are presented for
+# selection = 'alpha'
 plot(samples_LNR, selection = "alpha", by_subject = FALSE)
 plot(samples_LNR, selection = "alpha", by_subject = TRUE)
 
-# return_mcmc is more for other functions and not applicable for plot, but whether to return an mcmc object
-# merge_chains merges chains
+# merge_chains concatenates chains
 plot(samples_LNR, selection = "alpha", merge_chains = T)
+
 # subject picks out certain subjects
 plot(samples_LNR, selection = "alpha", subject = 1:2, by_subject = T)
 plot(samples_LNR, selection = "alpha", subject = 1:2, by_subject = F)
@@ -67,42 +80,49 @@ plot(samples_LNR, selection = "alpha", subject = "as1t", by_subject = F)
 
 # flatten determines how 3-d samples are presented (excluding selection = 'alpha')
 plot(samples_LNR, selection = "correlation", flatten = FALSE)
-plot(samples_LNR, selection = "correlation", flatten = TRUE) # flatten = T is the old way
+plot(samples_LNR, selection = "correlation", flatten = TRUE)
 
-# remove_dup removes duplicate samples, notices how the panels of correlations gets smaller and smaller
+# remove_dup removes duplicate samples, notice how the panels of correlations
+# gets smaller and smaller
 plot(samples_LNR, selection = "correlation", remove_dup = TRUE)
-# remove constants isn't relevant for plotting, but could I guess be done
+
+# remove constants isn't relevant for plotting, but can be turned off
 plot(samples_LNR, selection = "correlation", remove_constants = FALSE)
+
 # use_par can be used to select certain parameters
 plot(samples_LNR, selection = "mu", use_par = c("m", "s"))
-# or super-groups of parameters, will only plot 2 panels, the correlations with m and s
+# or sub-groups of parameters, her it will only plot 2 panels, the correlations
+# with m and s
 plot(samples_LNR, selection = "correlation", use_par = c("m", "s"))
 # also works with flatten
 plot(samples_LNR, selection = "correlation", use_par = c("s.m"), flatten = T)
 # and with map
 plot(samples_LNR, selection = "mu", map = T, use_par = "m_TRUE")
 
+# ????
 plot(samples_LNR, selection = "mu", map = T, use_par = "m_TRUE", add_recalculated = TRUE)
 
-# type is only for other functions, and so is true_pars
 # chain we can use to only take some chains
 plot(samples_LNR, selection = "mu", chain = 1:2)
 plot(samples_LNR, selection = "mu", chain = 3)
 
-# The new plotting functions now also take graphical parameters of base R
-# The old ones like plot_fit I still have to do that for
-# Just putting it all together to flex a little bit
-plot(samples_LNR, selection = "alpha", chain = 1:2, map = TRUE, use_par = c("m_TRUE", "s"),
-     subject = 1:2, filter = 20:50, length.out = 20, by_subject = TRUE, col = c("darkgreen", "purple"),
-     lwd = 2, lty = 1, main = "niek is flexing")
+# Plotting functions now also take graphical parameters of base R with a few
+# exceptions (e.g., plot_fit)
 
-# Something I'm still working on is having the plot arguments passable as a list, one for each plot
-# So for now you can't set separate ylim for each plot for example
+# For example putting amny options together
+plot(samples_LNR, selection = "alpha", chain = 1:2, map = TRUE,
+     use_par = c("m_TRUE", "s"),subject = 1:2, filter = 20:50, length.out = 20,
+     by_subject = TRUE, col = c("darkgreen", "purple"),
+     lwd = 2, lty = 1, main = "wow this is flexible")
+
+# Something we are still working on is having the plot arguments passable as a
+# list, one for each plot. So for now you can't set separate ylim for each plot,
+# for example.
 
 # Ok having discussed most of the new argument functionality let's go over the new functions
 
 # statistics --------------------------------------------------------------
-# Only the functions plot, check and summary take length(selection) > 1 (for now)
+# Only the functions plot, check, and summary take length(selection) > 1
 check(samples_LNR) # plots the worst parameter of each selection
 summ <- summary(samples_LNR) # By default prints the first subject
 # all subjects are stored in here though
@@ -133,8 +153,8 @@ plot_pars(samples_LNR, plot_prior = F)
 plot_pars(samples_LNR, col = "orange", xlim = c(-2, 2), lty = 3, lwd = 2,
           prior_plot_args = list(col = "purple", lty = 1))
 
-# With selection is alpha, the prior is now a rmvnorm draw using the group-level mean and
-# group-level variance
+# With selection is alpha, the prior is now a rmvnorm draw using the group-level
+# mean and group-level variance
 plot_pars(samples_LNR, selection = "alpha")
 
 # Can now also plot all subjects, which in this case is just 3
@@ -159,8 +179,8 @@ profile_plot(sim_dat, design, pmedian, use_par = c("m", "t0"),
 
 
 # prior -------------------------------------------------------------------
-# let's use that design to make a prior
-# I kind of want fill_default to be F
+# let's use that design to make a prior, here enter at the prompt to by
+# setting fill_default to be F
 prior_input <- prior(design, fill_default = F)
 
 # or use ask to only fill those
@@ -201,10 +221,6 @@ recovered
 # or for a vector instead of emc object
 recovered <- recovery(sim_emc, pmedian, selection = "mu")
 recovered
-
-
-# recovery ----------------------------------------------------------------
-# Not much changed here but I'll fill in later
 
 
 # more plots --------------------------------------------------------------
