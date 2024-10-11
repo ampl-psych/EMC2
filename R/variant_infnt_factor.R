068bb786ed40f1c238c83c7c8e4dcaba92dea76f
Niek Stevenson <niek.stevenson@gmail.com>
2024-10-11 16:24:24 +0200

    Changed small mistake in infnt_factor

--- R/variant_infnt_factor.R
+++ R/variant_infnt_factor.R
@@ -18,6 +18,9 @@ get_prior_infnt_factor <- function(prior = NULL, n_pars = NULL, sample = TRUE, N
   if(!is.null(design)){
     n_pars <- length(sampled_p_vector(design, doMap = F))
   }
+  if (!is.null(prior$theta_mu_mean)) {
+    n_pars <- length(prior$theta_mu_mean)
+  }
   if(is.null(prior$theta_mu_mean)){
     prior$theta_mu_mean <- rep(0, n_pars)
   }
