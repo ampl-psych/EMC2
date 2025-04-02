e6eb1fef13cbdea7d57db957487c60bbaa24eb9a
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2025-04-02 22:02:07 +0200

    Added parallel test function

---
+++ R/test.R
@@ -0,0 +1,15 @@
+donothing <- function(i) repeat{}
+
+pdonothing <- function(i)
+  mclapply(1:3,donothing,mc.cores=3)
+
+#' Title
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+test_parallel <- function() {
+  ncores=3
+  mclapply(1:ncores,pdonothing,mc.cores=ncores)
+}
