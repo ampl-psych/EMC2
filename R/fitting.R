60b20bd946eea9f7c1d4c0ad7072cd7a9a064871
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2025-04-03 08:44:09 +0200

    n

--- R/fitting.R
+++ R/fitting.R
@@ -119,12 +119,7 @@ run_emc <- function(emc, stage, stop_criteria,
       sub_emc <- subset(emc, filter = chain_n(emc)[1,stage] - 1, stage = stage)
     }
     # Actual sampling
-    # sub_emc <- auto_mclapply(sub_emc,run_stages, stage = stage, iter= progress$step_size*max(1,cur_thin),
-    #                          verbose=verbose,  verboseProgress = verboseProgress,
-    #                          particle_factor=particle_factor,search_width=search_width,
-    #                          n_cores=cores_per_chain, mc.cores = cores_for_chains)
-    test_parallel()
-    sub_emc <- parallel::mclapply(sub_emc,run_stages, stage = stage, iter= progress$step_size*max(1,cur_thin),
+    sub_emc <- auto_mclapply(sub_emc,run_stages, stage = stage, iter= progress$step_size*max(1,cur_thin),
                              verbose=verbose,  verboseProgress = verboseProgress,
                              particle_factor=particle_factor,search_width=search_width,
                              n_cores=cores_per_chain, mc.cores = cores_for_chains)
