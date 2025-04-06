fa651c9881a9cc83484b6a6eec990d91f4b3e5aa
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2025-04-06 08:07:03 +0200

    Fix bug add_accumulators to make sure levels are kept on the matchfun factor when already a factor.

--- R/design.R
+++ R/design.R
@@ -280,10 +280,16 @@ if (type=="DDM") {
     datar <- datar[order(rep(1:dim(data)[1],nacc),datar$lR),]
     if (!is.null(matchfun)) {
       lM <- matchfun(datar)
-      # if (any(is.na(lM)) || !(is.logical(lM)))
-      #   stop("matchfun not scoring properly")
-      datar$lM <- factor(lM)
+      if (!is.factor(lM))
+        datar$lM <- factor(lM) else
+        datar$lM <- factor(lM,levels=levels(lM))
     }
+    # if (!is.null(matchfun)) {
+    #   lM <- matchfun(datar)
+    #   # if (any(is.na(lM)) || !(is.logical(lM)))
+    #   #   stop("matchfun not scoring properly")
+    #   datar$lM <- factor(lM)
+    # }
     # Advantage NAFC
     nam <- unlist(lapply(strsplit(dimnames(datar)[[2]],"lS"),function(x)x[[1]]))
     islS <- nam ==""
