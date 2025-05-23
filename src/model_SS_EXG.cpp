4fa08e7ffe546ae895ecef32e9e0cbe3a6e22637
Frank Hezemans <frankhezemans@gmail.com>
2025-05-23 22:56:03 +0200

    minor tweaks for readability

--- src/model_SS_EXG.cpp
+++ src/model_SS_EXG.cpp
@@ -5,51 +5,66 @@ using namespace Rcpp;
 
 
 // [[Rcpp::export]]
-NumericVector pEXG(NumericVector q,
-                   double mu = 5., double sigma = 1., double tau = 1.,
-                   bool lower_tail = true, bool log_p = false) {
+NumericVector pEXG(
+    NumericVector q,
+    double mu = 5.,
+    double sigma = 1.,
+    double tau = 1.,
+    bool lower_tail = true,
+    bool log_p = false
+) {
+
   int n = q.size();
-  if (tau <= 0 || sigma <= 0) {
-    NumericVector cdf(n, NA_REAL);
+  NumericVector cdf(n);
+
+  // return NA for invalid parameter values
+  if (sigma <= 0 || tau <= 0) {
+    std::fill(cdf.begin(), cdf.end(), NA_REAL);
     return cdf;
   }
 
-  NumericVector cdf(n);
-  if (sigma < 1e-4){
-    for (int i = 0; i < n; i++){
+  // if Gaussian SD is practically zero, treat as shifted exponential
+  if (sigma < 1e-4) {
+    for (int i = 0; i < n; i++) {
       cdf[i] = R::pexp(q[i] - mu, tau, lower_tail, log_p);
     }
     return cdf;
   }
 
-  for (int i = 0; i < n; i++){
-    if (!traits::is_infinite<REALSXP>(q[i])){
-      if (tau > .05 * sigma){
-        double z_i = q[i] - mu - (sigma * sigma) / tau;
-        cdf[i] = R::pnorm((q[i] - mu) / sigma, 0., 1., true, false) - std::exp(std::log(R::pnorm(z_i / sigma, 0., 1., true, false)) + (std::pow((mu + (sigma * sigma / tau)), 2) - mu * mu - 2. * q[i] * (sigma * sigma / tau)) / (2. * sigma * sigma));
+  for (int i = 0; i < n; i++) {
+    if (!traits::is_infinite<REALSXP>(q[i])) {
+      if (tau > .05 * sigma) {
+        // if exponential rate has reasonable value, treat as ex-Gaussian
+        double delta = sigma * sigma / tau;
+        double z = q[i] - mu - delta;
+        double norm1 = R::pnorm((q[i] - mu) / sigma, 0., 1., true, false);
+        double norm2 = R::pnorm(z / sigma, 0., 1., true, false);
+        double exponent = (
+          std::pow(mu + delta, 2.) - mu * mu - 2. * q[i] * delta
+        ) / (2. * sigma * sigma);
+        cdf[i] = norm1 - std::exp(std::log(norm2) + exponent);
       } else {
+        // if not, treat as Gaussian
         cdf[i] = R::pnorm(q[i], mu, sigma, true, false);
       }
     } else {
-      if (q[i] < 0) {
-        cdf[i] = 0.;
-      } else {
-        cdf[i] = 1.;
-      }
+      // handle negative and positive infinity input q
+      cdf[i] = (q[i] < 0.0) ? 0. : 1.;
     }
   }
-  if (!lower_tail){
-    for(int i = 0; i < n; i++){
-      cdf[i] = 1. - cdf[i];
+
+  if (!lower_tail || log_p) {
+    for (int i = 0; i < n; i++) {
+      // complementary CDF
+      if (!lower_tail) cdf[i] = 1. - cdf[i];
+      // log transform
+      if (log_p) cdf[i] = std::log(cdf[i]);
     }
   }
-  if (log_p){
-    for(int i = 0; i < n; i++){
-      cdf[i] = std::log(cdf[i]);
-    }
-  }
+
   return cdf;
 }
+
 
 // [[Rcpp::export]]
 NumericVector dEXG(NumericVector x,
