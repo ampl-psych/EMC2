cbba82bab7f4a08c0418a07bd0f4af4e2bd72280
Frank Hezemans <frankhezemans@gmail.com>
2025-06-03 12:03:40 +0200

    intermediate computations in log space

--- src/race_integrate.h
+++ src/race_integrate.h
@@ -17,48 +17,43 @@ struct IntegrationResult {
 
 class race_f : public Func {
 private:
-  NumericMatrix pars;
-  LogicalVector winner;
-  NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double);
-  NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double);
-  double min_ll;
-  double min_p;
+  const NumericMatrix pars;
+  const LogicalVector winner;
+  NumericVector (*ldfun)(NumericVector, NumericMatrix, LogicalVector, double);
+  NumericVector (*lccdfun)(NumericVector, NumericMatrix, LogicalVector, double);
+  const double min_ll;
 
 public:
   race_f(
     NumericMatrix pars_,
     LogicalVector winner_,
-    NumericVector (*dfun_)(NumericVector, NumericMatrix, LogicalVector, double),
-    NumericVector (*pfun_)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*ldfun_)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*lccdfun_)(NumericVector, NumericMatrix, LogicalVector, double),
     double min_ll_
   ) :
   pars(pars_),
   winner(winner_),
-  dfun(dfun_),
-  pfun(pfun_),
-  min_ll(min_ll_),
-  min_p(std::exp(min_ll_)) {}
+  ldfun(ldfun_),
+  lccdfun(lccdfun_),
+  min_ll(min_ll_) {}
 
-  // integrand definition
   double operator()(const double& x) const {
     int n_acc = pars.nrow();
     NumericVector t(n_acc, x);
-    // compute probability of winner accumulator finishing at time t
-    NumericVector d = dfun(t, pars, winner, min_p);
-    if (d.size() != 1) {
-      stop("dfun must return a scalar NumericVector of length 1");
+    // compute log density of winner accumulator finishing at time t
+    NumericVector log_d = ldfun(t, pars, winner, min_ll);
+    if (log_d.size() != 1) {
+      stop("ldfun must return a scalar NumericVector of length 1");
     }
-    double out = d[0];
-    // multiply by probability of loser accumulator(s) not having finished by
-    // time t
+    double log_out = log_d[0];
+    // (summed) log survival probability of losing accumulator(s)
     if (n_acc > 1) {
-      NumericVector p = 1. - pfun(t, pars, !winner, min_p);
-      double prod_p = std::accumulate(p.begin(), p.end(), 1., std::multiplies<double>());
-      out *= prod_p;
+      NumericVector log_s = lccdfun(t, pars, !winner, min_ll);
+      log_out += std::accumulate(log_s.begin(), log_s.end(), 0.);
     }
     // output is instantaneous likelihood that winner accumulator finishes at
     // time t, before the loser accumulator(s).
-    return out;
+    return std::exp(log_out);
   }
 };
 
@@ -66,8 +61,8 @@ public:
 IntegrationResult my_integrate(
     NumericMatrix pars,
     LogicalVector winner,
-    NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double),
-    NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*ldfun)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*lccdfun)(NumericVector, NumericMatrix, LogicalVector, double),
     double min_ll,
     double lower,
     double upper,
@@ -76,7 +71,7 @@ IntegrationResult my_integrate(
     double fail_out = 0.
 ) {
   // set up the integrand (race model function)
-  race_f f(pars, winner, dfun, pfun, min_ll);
+  race_f f(pars, winner, ldfun, lccdfun, min_ll);
   double err_est;
   int err_code;
 
@@ -112,18 +107,18 @@ IntegrationResult my_integrate(
 double pr_pt(
     NumericMatrix pars,
     LogicalVector winner,
-    NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double),
-    NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*ldfun)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*lccdfun)(NumericVector, NumericMatrix, LogicalVector, double),
     double min_ll,
     double LT,
     double UT
 ) {
   // probability in full response window (zero to positive infinity)
-  IntegrationResult pr = my_integrate(pars, winner, dfun, pfun, min_ll, 0., R_PosInf);
+  IntegrationResult pr = my_integrate(pars, winner, ldfun, lccdfun, min_ll, 0., R_PosInf);
   if (pr.error_code != 0 || traits::is_nan<REALSXP>(pr.value)) return NA_REAL;
 
   // probability in truncated response window [LT, UT]
-  IntegrationResult pt = my_integrate(pars, winner, dfun, pfun, min_ll, LT, UT);
+  IntegrationResult pt = my_integrate(pars, winner, ldfun, lccdfun, min_ll, LT, UT);
   if (pt.error_code != 0 || traits::is_nan<REALSXP>(pt.value)) return NA_REAL;
 
   // handle edge cases before computing and returning ratio of probabilities
@@ -140,8 +135,8 @@ double pr_pt(
 double pLU(
     NumericMatrix pars,
     LogicalVector winner,
-    NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double),
-    NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*ldfun)(NumericVector, NumericMatrix, LogicalVector, double),
+    NumericVector (*lccdfun)(NumericVector, NumericMatrix, LogicalVector, double),
     double min_ll,
     double LT,
     double LC,
@@ -149,11 +144,11 @@ double pLU(
     double UT
 ) {
   // probability in [LT, LC]
-  IntegrationResult pL = my_integrate(pars, winner, dfun, pfun, min_ll, LT, LC);
+  IntegrationResult pL = my_integrate(pars, winner, ldfun, lccdfun, min_ll, LT, LC);
   if (pL.error_code != 0 || traits::is_nan<REALSXP>(pL.value)) return NA_REAL;
 
   // probability in [UC, UT]
-  IntegrationResult pU = my_integrate(pars, winner, dfun, pfun, min_ll, UC, UT);
+  IntegrationResult pU = my_integrate(pars, winner, ldfun, lccdfun, min_ll, UC, UT);
   if (pU.error_code != 0 || traits::is_nan<REALSXP>(pU.value)) return NA_REAL;
 
   // clamp probabilities to [0, 1] and return summed probability
