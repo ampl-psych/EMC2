2c920dee7cf701be81384b879677c32a1ca6e319
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2025-07-08 13:21:32 +1000

    Made _jeroen version of race_f, replaced franks updates of pLU, pr_pt with Jeroen's versions, and added Jeroen's f_integrate and f_integrate_slow functions

--- src/race_integrate.h
+++ src/race_integrate.h
@@ -65,6 +65,45 @@ public:
 };
 
 
+class race_f_jeroen : public Func {
+private:
+  Rcpp::NumericMatrix pars;
+  Rcpp::LogicalVector winner;
+  Rcpp::NumericVector (*dfun)(Rcpp::NumericVector, Rcpp::NumericMatrix, Rcpp::LogicalVector, double);
+  Rcpp::NumericVector (*pfun)(Rcpp::NumericVector, Rcpp::NumericMatrix, Rcpp::LogicalVector, double);
+  double min_ll;
+
+public:
+  race_f_jeroen(Rcpp::NumericMatrix pars_,
+        Rcpp::LogicalVector winner_,
+        Rcpp::NumericVector (*dfun_)(Rcpp::NumericVector, Rcpp::NumericMatrix, Rcpp::LogicalVector, double),
+        Rcpp::NumericVector (*pfun_)(Rcpp::NumericVector, Rcpp::NumericMatrix, Rcpp::LogicalVector, double),
+        double min_ll_) :
+  pars(pars_),
+  winner(winner_),
+  dfun(dfun_),
+  pfun(pfun_),
+  min_ll(min_ll_) {}
+
+  double operator()(const double& x) const
+  {
+    double accumulators = pars.nrow();
+    NumericVector t(accumulators);
+    t.fill(x);
+    Rcpp::NumericVector d = dfun(t, pars, winner, exp(min_ll));
+    double out = Rcpp::as<double>(d);
+
+    if (accumulators > 1) {
+      Rcpp::NumericVector p = 1 - pfun(t, pars, !winner, exp(min_ll));
+      double prod_p = std::accumulate(p.begin(), p.end(), 1.0,
+                                      std::multiplies<double>());
+      out *= prod_p;
+    }
+    return out;
+  }
+};
+
+
 IntegrationResult my_integrate(
     const Func& race_pdf,
     double lower,
@@ -115,52 +154,90 @@ IntegrationResult my_integrate(
   return {res, err_est, err_code};
 }
 
+//Jeroen
+NumericVector f_integrate(Rcpp::NumericMatrix pars,
+                                Rcpp::LogicalVector winner,
+                                Rcpp::NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double),
+                                Rcpp::NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double),
+                                double min_ll,
+                                double lower,
+                                double upper)
+{
+  race_f_jeroen f(pars, winner, dfun, pfun, min_ll);
+  double err_est;
+  int err_code;
+  double res = integrate(f, lower, upper, err_est, err_code);
+  NumericVector out{res, err_est, (double) err_code};
+  return out;
+}
 
-double pr_pt(
-    const Func& race_pdf,
-    double LT,
-    double UT
-) {
-  // probability in full response window (zero to positive infinity)
-  IntegrationResult pr = my_integrate(race_pdf, 0., R_PosInf);
-  if (pr.error_code != 0 || traits::is_nan<REALSXP>(pr.value)) return NA_REAL;
+NumericVector f_integrate_slow(Rcpp::NumericMatrix pars,
+                          Rcpp::LogicalVector winner,
+                          Rcpp::NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double),
+                          Rcpp::NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double),
+                          double min_ll,
+                          double lower,
+                          double upper)
+{
+  race_f_jeroen f(pars, winner, dfun, pfun, min_ll);
+  double err_est;
+  int err_code;
+  double res = integrate(f, lower, upper, err_est, err_code);
+  if (err_code == 1 && upper == R_PosInf) {
+    double err_est_hacky;
+    int err_code_hacky;
+    double res_hacky = integrate(f, lower, 10, err_est_hacky, err_code_hacky);
+    NumericVector out{res_hacky, err_est_hacky, (double) err_code_hacky};
+    return out;
+  } else if (err_code > 0) {
+    NumericVector out{min_ll, err_est, (double) err_code};
+    return out;
+  } else {
+    NumericVector out{res, err_est, (double) err_code};
+    return out;
+  }
+}
 
-  // probability in truncated response window [LT, UT]
-  IntegrationResult pt = my_integrate(race_pdf, LT, UT);
-  if (pt.error_code != 0 || traits::is_nan<REALSXP>(pt.value)) return NA_REAL;
+double pr_pt(Rcpp::NumericMatrix pars,
+             Rcpp::LogicalVector winner,
+             Rcpp::NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double),
+             Rcpp::NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double),
+             double min_ll,
+             double LT,
+             double UT) {
+  NumericVector pr = f_integrate(pars, winner, dfun, pfun, min_ll, 0, R_PosInf);
+  if ((pr[2] != 0) || traits::is_nan<REALSXP>(pr[0])) return NA_REAL;
+  if (pr[0] == 0.0) return 0.0;
 
-  // handle edge cases before computing and returning ratio of probabilities
-  if (pt.value == 0.) return NA_REAL;
-  if (pr.value == 0.) return 0.;
+  NumericVector pt = f_integrate(pars, winner, dfun, pfun, min_ll, LT, UT);
+  if ((pt[2] != 0) || traits::is_nan<REALSXP>(pt[0])) return NA_REAL;
 
-  double out = pr.value / pt.value;
+  double out = std::max(0.0, std::min(pr[0], 1.0)) / std::max(0.0, std::min(pt[0], 1.0));
   if (traits::is_infinite<REALSXP>(out)) return NA_REAL;
 
-  return std::max(0., std::min(1., out));
+  return out;
 }
 
 
-double pLU(
-    const Func& race_pdf,
-    double LT,
-    double LC,
-    double UC,
-    double UT
-) {
-  // probability in [LT, LC]
-  IntegrationResult pL = my_integrate(race_pdf, LT, LC);
-  if (pL.error_code != 0 || traits::is_nan<REALSXP>(pL.value)) return NA_REAL;
-
-  // probability in [UC, UT]
-  IntegrationResult pU = my_integrate(race_pdf, UC, UT);
-  if (pU.error_code != 0 || traits::is_nan<REALSXP>(pU.value)) return NA_REAL;
-
-  // clamp probabilities to [0, 1] and return summed probability
-  // out = std::max(0., std::min(1., out));
-  double clampedL = std::max(0., std::min(1., pL.value));
-  double clampedU = std::max(0., std::min(1., pU.value));
-
-  return clampedL + clampedU;
+double pLU(Rcpp::NumericMatrix pars,
+           Rcpp::LogicalVector winner,
+           Rcpp::NumericVector (*dfun)(NumericVector, NumericMatrix, LogicalVector, double),
+           Rcpp::NumericVector (*pfun)(NumericVector, NumericMatrix, LogicalVector, double),
+           double min_ll,
+           double LT,
+           double LC,
+           double UC,
+           double UT) {
+  NumericVector pL = f_integrate(pars, winner, dfun, pfun, min_ll, LT, LC);
+  if ((pL[2] != 0) | traits::is_nan<REALSXP>(pL[0])) {
+    return NA_REAL;
+  }
+  NumericVector pU = f_integrate(pars, winner, dfun, pfun, min_ll, UC, UT);
+  if ((pU[2] != 0) | traits::is_nan<REALSXP>(pU[0])) {
+    return NA_REAL;
+  }
+  double out = std::max(0.0, std::min(pL[0], 1.0)) + std::max(0.0, std::min(pU[0], 1.0));
+  return out;
 }
 
 #endif
