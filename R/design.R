144a312faeec9f0a4bf581c80974d58fad2b8895
Steven Miletić <steven@miletic.nl>
2025-07-09 17:32:42 +0200

    bug fix, idiot

--- R/design.R
+++ R/design.R
@@ -607,7 +607,7 @@ design_model <- function(data,design,model=NULL,
           if(trend[[trend_name]]$filter_lR) {
             cov_name <- gsub('_lRfiltered', '', trend[[trend_name]]$covariate)
             # copy to new column, filter
-            dadm[,trend[[trend_name]]$covariate] <- dadm[dadm$lR!=levels(dadm$lR)[1],cov_name]
+            dadm[,trend[[trend_name]]$covariate] <- dadm[,cov_name]
             dadm[dadm$lR!=levels(dadm$lR)[1],trend[[trend_name]]$covariate] <- NA
           }
         }
