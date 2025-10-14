60f58f3c76155e51568cf3b4c29b5fd937c8412b
Frank Hezemans <frankhezemans@gmail.com>
2025-10-14 23:45:50 +0200

    minor edits to SBC plot functions: ensure `add_stats` arg is actually used

--- R/SBC.R
+++ R/SBC.R
@@ -229,8 +229,12 @@ calc_sbc_stats <- function(stats){
 #' @return No returns
 #' @export
 plot_sbc_hist <- function(ranks, bins = 10, layout = NA, add_stats = TRUE){
-  if(!is.null(ranks$rank)) stats <- calc_sbc_stats(ranks)
-  if(!is.null(ranks$rank)) ranks <- ranks$rank
+  if (!is.null(ranks[["rank"]])) {
+    stats <- calc_sbc_stats(ranks)
+  }
+  if (!is.null(ranks[["rank"]])) {
+    ranks <- ranks[["rank"]]
+  }
   selects <- names(ranks)
   oldpar <- par(no.readonly = TRUE) # code line i
   on.exit(par(oldpar)) # code line i + 1
@@ -240,26 +244,47 @@ plot_sbc_hist <- function(ranks, bins = 10, layout = NA, add_stats = TRUE){
   mid <- qbinom(0.5, n_sample, 1/bins)
   high <- qbinom(0.975, n_sample, 1/bins)
   par_names <- colnames(ranks[[1]])
-  for(j in 1:length(ranks)){
-    if(any(is.na(layout))){
-      par(mfrow = coda_setmfrow(Nchains = 1, Nparms = ncol(ranks[[1]]),
-                                       nplots = 1))
-    } else{par(mfrow=layout)}
+  for (j in seq_along(ranks)) {
+    if (any(is.na(layout))) {
+      par(
+        mfrow = coda_setmfrow(
+          Nchains = 1, Nparms = ncol(ranks[[1]]), nplots = 1
+        )
+      )
+    } else {
+      par(mfrow = layout)
+    }
     rank <- ranks[[j]]
     stat <- stats[[j]]
-
-    for(i in 1:ncol(rank)){
-      hist(rank[,i], main = paste0(selects[j], " - ", par_names[i]), breaks = bins, ylim = c(0, high + 2), xlab = "rank")
+    for (i in 1:ncol(rank)) {
+      hist(
+        x = rank[ , i],
+        main = paste0(selects[j], " - ", par_names[i]),
+        breaks = bins,
+        ylim = c(0, high + 2),
+        xlab = "rank"
+      )
       abline(h = low, lty = 2)
       abline(h = mid, lty = 2)
       abline(h = high, lty = 2)
-      if(!is.null(stat)){
-        legend("topleft",legend=paste0("coverage : ",round(stat$coverage[i],2)), bty = "n")
-        legend("top",legend=paste0("bias : ",round(stat$bias[i],3)), bty = "n")
-        legend("topright",legend=paste0("precision : ",round(stat$precision[i],3)), bty = "n")
+      if (!is.null(stat) && add_stats) {
+        legend(
+          x = "topleft",
+          legend = paste0("coverage : ", round(stat[["coverage"]][i], 2)),
+          bty = "n"
+        )
+        legend(
+          x = "top",
+          legend = paste0("bias : ", round(stat[["bias"]][i], 3)),
+          bty = "n"
+        )
+        legend(
+          x = "topright",
+          legend = paste0("precision : ", round(stat[["precision"]][i], 3)),
+          bty = "n"
+        )
       }
     }
-
   }
 }
 
@@ -324,8 +349,12 @@ make_smooth <- function(x, y, N = 1000){
 #' @return No returns
 #' @export
 plot_sbc_ecdf <- function(ranks, layout = NA, add_stats = TRUE){
-  if(!is.null(ranks$rank)) stats <- calc_sbc_stats(ranks)
-  if(!is.null(ranks$rank)) ranks <- ranks$rank
+  if (!is.null(ranks[["rank"]])) {
+    stats <- calc_sbc_stats(ranks)
+  }
+  if (!is.null(ranks[["rank"]])) {
+    ranks <- ranks[["rank"]]
+  }
   selects <- names(ranks)
   oldpar <- par(no.readonly = TRUE) # code line i
   on.exit(par(oldpar)) # code line i + 1
@@ -333,24 +362,56 @@ plot_sbc_ecdf <- function(ranks, layout = NA, add_stats = TRUE){
   K <- N <- nrow(ranks[[1]])
   gamma <- get_gamma(N, K)
   res <- get_lims(N, K, gamma)
-  for(j in 1:length(ranks)){
-    if(any(is.na(layout))){
-      par(mfrow = coda_setmfrow(Nchains = 1, Nparms = ncol(ranks[[1]]),
-                                nplots = length(ranks)))
-    } else{par(mfrow=layout)}
-
+  for (j in seq_along(ranks)) {
+    if (any(is.na(layout))) {
+      par(
+        mfrow = coda_setmfrow(
+          Nchains = 1, Nparms = ncol(ranks[[1]]), nplots = length(ranks)
+        )
+      )
+    } else {
+      par(mfrow = layout)
+    }
     rank <- ranks[[j]]
     stat <- stats[[j]]
     par_names <- colnames(rank)
-    res$x <- apply(rank, 2, function(x) sort(x) - res$z)
-    for(i in 1:ncol(rank)){
-      plot(res$z, res$x[,i], type = "l", ylim = c(min(res$lower, res$x[,i]) - 0.01, max(res$upper,res$x[,i]) + 0.01), xlim = c(0, 1),
-           lwd = 2, ylab = "ECDF Difference", xlab = "Normalized Rank Statistic", main = paste0(selects[j], " - ", par_names[i]))
-      polygon(c(res$z, rev(res$z)), c(res$lower, rev(res$upper)), col = adjustcolor("cornflowerblue", 0.2))
-      if(!is.null(stat)){
-        legend("topleft",legend=paste0("coverage : ",round(stat$coverage[i],2)), bty = "n")
-        legend("bottomright",legend=paste0("bias : ",round(stat$bias[i],3)), bty = "n")
-        legend("topright",legend=paste0("precision : ",round(stat$precision[i],3)), bty = "n")
+    res[["x"]] <- apply(rank, 2, function(x) {sort(x) - res[["z"]]})
+    for (i in 1:ncol(rank)) {
+      plot(
+        x = res[["z"]],
+        y = res[["x"]][ , i],
+        type = "l",
+        lwd = 2,
+        ylim = c(
+          min(res[["lower"]], res[["x"]][ , i]) - 0.01,
+          max(res[["upper"]], res[["x"]][ , i]) + 0.01
+        ),
+        xlim = c(0, 1),
+        ylab = "ECDF Difference",
+        xlab = "Normalized Rank Statistic",
+        main = paste0(selects[j], " - ", par_names[i])
+      )
+      polygon(
+        x = c(res[["z"]], rev(res[["z"]])),
+        y = c(res[["lower"]], rev(res[["upper"]])),
+        col = adjustcolor("cornflowerblue", 0.2)
+      )
+      if (!is.null(stat) && add_stats) {
+        legend(
+          x = "topleft",
+          legend = paste0("coverage : ", round(stat[["coverage"]][i], 2)),
+          bty = "n"
+        )
+        legend(
+          x = "bottomright",
+          legend = paste0("bias : ", round(stat[["bias"]][i], 3)),
+          bty = "n"
+        )
+        legend(
+          x = "topright",
+          legend = paste0("precision : ", round(stat[["precision"]][i], 3)),
+          bty = "n"
+        )
       }
     }
   }
