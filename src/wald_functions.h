b706a6aeb3b180d3ca47c469bb82018eea009c4c
Frank Hezemans <frankhezemans@gmail.com>
2025-11-10 14:29:34 +0100

    bugfix: move args around

--- src/wald_functions.h
+++ src/wald_functions.h
@@ -196,16 +196,16 @@ double pigt(
     double k = 1,
     double l = 1,
     double a = .1,
-    double threshold = 1e-10,
     bool lower_tail = true,
-    bool log_p = false
+    bool log_p = false,
+    double threshold = 1e-10
 ){
-  if (t <= 0.){
+  if (t <= 0.) {
     return 0.;
   }
   double cdf;
 
-  if (a < threshold){
+  if (a < threshold) {
     return pwald(t, l, k, lower_tail, log_p);
     // cdf = pigt0(t, k, l);
     // if (!lower_tail) {
@@ -220,24 +220,19 @@ double pigt(
   if (l < threshold){
     double t5a = 2. * R::pnorm((k + a) / sqt, 0., 1., true, false) - 1;
     double t5b = 2. * R::pnorm((- k - a) / sqt, 0., 1., true, false) - 1;
-
     double t6a = - .5 * ((k + a) * (k + a) / t - M_LN2 - L_PI + lgt) - std::log(a);
     double t6b = - .5 * ((k - a) * (k - a) / t - M_LN2 - L_PI + lgt) - std::log(a);
-
     cdf = 1. + std::exp(t6a) - std::exp(t6b) + ((- k + a) * t5a - (k - a) * t5b) / (2. * a);
   } else {
     double t1a = std::exp(- .5 * std::pow(k - a - t * l, 2) / t);
     double t1b = std::exp(- .5 * std::pow(a + k - t * l, 2) / t);
     double t1 = std::exp(.5* (lgt - M_LN2 - L_PI)) * (t1a - t1b);
-
     double t2a = std::exp(2. * l * (k - a) + R::pnorm(- (k - a + t * l) / sqt, 0., 1., true, true));
     double t2b = std::exp(2. * l * (k + a) + R::pnorm(- (k + a + t * l) / sqt, 0., 1., true, true));
     double t2 = a + (t2b - t2a) / (2. * l);
-
     double t4a = 2. * R::pnorm((k + a) / sqt - sqt * l, 0., 1., true, false) - 1.;
     double t4b = 2. * R::pnorm((k - a) / sqt - sqt * l, 0., 1., true, false) - 1.;
     double t4 = .5 * (t * l - a - k + .5 / l) * t4a + .5 * (k - a - t * l - .5 / l) * t4b;
-
     cdf = .5 * (t4 + t2 + t1) / a;
   }
 
@@ -259,15 +254,15 @@ double digt(
     double k = 1.,
     double l = 1.,
     double a = .1,
-    double threshold= 1e-10,
-    bool log_d = false
+    bool log_d = false,
+    double threshold = 1e-10
 ){
-  if (t <= 0.){
+  if (t <= 0.) {
     return 0.;
   }
   double pdf;
 
-  if (a < threshold){
+  if (a < threshold) {
     return dwald(t, l, k, log_d);
     // pdf = digt0(t, k, l);
     // return log_d? std::log(pdf) : pdf;
@@ -278,15 +273,12 @@ double digt(
     pdf = std::exp(-.5 * (M_LN2 + L_PI + std::log(t)) + std::log(term) - M_LN2 - std::log(a));
   } else {
     double sqt = std::sqrt(t);
-
     double t1a = - std::pow(a - k + t * l, 2) / (2. * t);
     double t1b = - std::pow(a + k - t * l, 2) / (2. * t);
     double t1 = M_SQRT1_2 * (std::exp(t1a) - std::exp(t1b)) / (std::sqrt(M_PI) * sqt);
-
     double t2a = 2. * R::pnorm((- k + a) / sqt + sqt * l, 0., 1., true, false) - 1.;
     double t2b = 2. * R::pnorm((k + a) / sqt - sqt * l, 0., 1., true, false) - 1.;
     double t2 = std::exp(std::log(.5) + std::log(l)) * (t2a + t2b);
-
     pdf = std::exp(std::log(t1 + t2) - M_LN2 - std::log(a));
   }
 
