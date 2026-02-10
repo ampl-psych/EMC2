46702295d19358a9db1db03743ff698107a8b2aa
Steven Miletić <steven@miletic.nl>
2026-02-10 15:59:41 +0100

    Clean-up

--- src/ParamTable.cpp
+++ src/ParamTable.cpp
@@ -1,43 +1,5 @@
 #include "ParamTable.h"
 
-// Helper: all parameter names in pt that are NOT in premap_trend_params
-// std::unordered_set<std::string>
-// remaining_nontrend_params(const ParamTable& pt,
-//                           const std::unordered_set<std::string>& premap_trend_params)
-// {
-//   std::unordered_set<std::string> out;
-//   const int p = pt.base_names.size();
-//   out.reserve(p);
-//
-//   for (int j = 0; j < p; ++j) {
-//     std::string nm = Rcpp::as<std::string>(pt.base_names[j]);
-//     if (premap_trend_params.find(nm) == premap_trend_params.end()) {
-//       out.insert(nm);
-//     }
-//   }
-//   return out;
-// }
-//
-// // Helper: all parameter names in pt that are NOT in premap_trend_params OR in pretransform_trend_params
-// std::unordered_set<std::string>
-//   remaining_nontrend_params(const ParamTable& pt,
-//                             const std::unordered_set<std::string>& premap_trend_params,
-//                             const std::unordered_set<std::string>& pretransform_trend_params)
-//   {
-//     std::unordered_set<std::string> out;
-//     const int p = pt.base_names.size();
-//     out.reserve(p);
-//
-//     for (int j = 0; j < p; ++j) {
-//       std::string nm = Rcpp::as<std::string>(pt.base_names[j]);
-//       // skip if in *either* premap or pretransform set
-//       if (premap_trend_params.find(nm) != premap_trend_params.end()) continue;
-//       if (pretransform_trend_params.find(nm) != pretransform_trend_params.end()) continue;
-//       out.insert(std::move(nm));
-//     }
-//     return out;
-//   }
-
 
 Rcpp::CharacterVector names_excluding(const Rcpp::CharacterVector& names,
                                      std::initializer_list<const std::unordered_set<std::string>*> excludes)
